4000009e90f2e79dc8479338e083088431e62f7d
alexsmirnov <alexeys@interlink-ua.com>
2018-08-03 18:27:15 +0100

    Added Add_Lead modal window

--- client/src/components/layouts/Home.js
+++ client/src/components/layouts/Home.js
@@ -2,12 +2,14 @@ import React, { Component } from "react";
 
 import Navbar from "./Navbar";
 import Dashboard from "../Dashboard";
+import AddLead from "../modal/AddLead";
 
 class Home extends Component {
   render() {
     return (
       <div>
         <Navbar />
+        <AddLead />
         <Dashboard />
       </div>
     );
