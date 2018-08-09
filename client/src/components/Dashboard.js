875bd89cc1e309c7692a10fad5406aec96ddd557
Poshtarenko Nazar <nazarpo@softwareplanet.uk.com>
2018-08-09 13:31:04 +0300

    moved Dashboard component's styles from App.css to Dashboard.css, added view count of leads in every stage, added isStagesEmpty function

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { loadLeadboard } from "../actions/leadActions";
+import "./Dashboard.css";
 
 import Lead from "./Lead";
 
@@ -10,7 +11,6 @@ class Dashboard extends Component {
     super();
 
     this.leadboardLoaded = false;
-    this.getLeads = this.getLeads.bind(this);
   }
 
   componentWillReceiveProps(nextProps) {
@@ -34,25 +34,51 @@ class Dashboard extends Component {
     }
   }
 
-  getLeads(stage, this_ = this) {
-    if (typeof this_.props.leads.leads["_" + stage] === "undefined") return <div />;
-    const leads = this_.props.leads.leads["_" + stage].leads.map(lead => {
-      return <Lead key={lead._id} lead={{ name: lead.name, company: "SPG_" }} />;
+  isStagesEmpty = (stages) => {
+    let isEmpty = true;
+    let stagesArray = Object.values(stages);
+    stagesArray.forEach( stage => {
+      if(stage.leads.length !== 0)
+        isEmpty = false;
     });
+    return isEmpty;
+  };
 
+  getLeads = stage => {
+    let leads;
+    if (typeof this.props.leads.leads["_" + stage] === "undefined" || this.isStagesEmpty(this.props.leads.leads))
+      leads = <h1>Blabal</h1>;
+    else
+      leads = this.props.leads.leads["_" + stage].leads.map(lead => {
+        return <Lead key={lead._id} lead={{ name: lead.name, company: "SPG_" }}/>;
+      });
     return leads;
-  }
+  };
 
   render() {
-    var stages = this.props.leads.stages.map(function(stage) {
+    let stages = this.props.leads.stages.map(stage => {
+      let leads = this.getLeads(stage._id);
       return (
         <div className="dashboard__stage" key={stage._id}>
-          <div className="dashboard__head">{stage.name}</div>
-          {this.getLeads(stage._id, this)}
-          <div className="dashboard__stage__card-terminator" />
+          <div className="dashboard__head">
+            <div>
+              <span className="stage__name">{stage.name}</span>
+              {
+                leads.length === 0 || !Array.isArray(leads) ? <span className="stage__value" /> : (
+                  <span className="stage__value">
+                  <small>{leads.length} {leads.length === 1 ? "lead" : "leads"}</small>
+                </span>
+                )
+              }
+            </div>
+          </div>
+          {
+            leads
+          }
+          <div className="dashboard__stage__card-terminator"/>
         </div>
       );
-    }, this);
+    });
 
     return <div className="dashboard">{stages}</div>;
   }
