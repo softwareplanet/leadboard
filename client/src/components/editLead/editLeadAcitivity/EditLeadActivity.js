021b40878aeba9126d23b6313487495e1b78e5f4
SamardakOleh <olehsa@softwareplanet.uk.com>
2018-08-14 16:25:40 +0300

    refactor directory names

--- client/src/components/editLead/editLeadAcitivity/EditLeadActivity.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from "react";
-import "./EditLeadActivity.css";
-
-export default class EditLeadActivity extends Component {
-  render() {
-    return <div className="edit-lead-activity">EditLeadActivity</div>;
-  }
-}
--- client/src/components/editLead/editLeadSidebar/EditLeadSidebar.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from "react";
-import "./EditLeadSidebar.css";
-
-export default class EditLeadSidebar extends Component {
-  render() {
-    return <div className="edit-lead-sidebar">EditLeadSidebar</div>;
-  }
-}
