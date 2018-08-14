6148a932fa3574699ee565a40357a23c0b497baa
Valentyn Tulub <valentyntu@softwareplanet.uk.com>
2018-08-14 10:52:07 +0300

    add backend test for creating default funnel and leads; fix tests to pass validation;

--- validation/lead.js
+++ validation/lead.js
@@ -7,7 +7,6 @@ module.exports = function validateLeadInput(data) {
   if (isEmpty(data.owner)) errors.owner = "Owner ID cannot be empty";
   if (isEmpty(data.stage)) errors.stage = "Stage ID cannot be empty";
 
-  console.log(typeof  data.order);
   if (typeof data.order !== "number") {
     errors.order = "Order must be a number";
   }
