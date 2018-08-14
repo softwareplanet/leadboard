f4d5412dcec181fb7d69c9045052e3a6dcd8658e
SamardakOleh <olehsa@softwareplanet.uk.com>
2018-08-14 13:04:26 +0300

    add status to lead model

--- models/lead.js
+++ models/lead.js
@@ -2,14 +2,18 @@ const mongoose = require("mongoose");
 
 const leadSchema = new mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
-  stage: { type: mongoose.Schema.Types.ObjectId, ref: 'Stage' },
+  stage: { type: mongoose.Schema.Types.ObjectId, ref: "Stage" },
   owner: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
   visibility: Number,
   name: String,
   order: { type: Number, index: { unique: false } },
   contact: { type: mongoose.Schema.Types.ObjectId, ref: "Contact" },
   custom: [{ name: "string", value: "string" }],
-  timestamp: { type: Date, default: Date.now }
+  timestamp: { type: Date, default: Date.now },
+  status: {
+    type: String,
+    enum: ["Won", "Lost", "InProgress"]
+  }
 });
 
 module.exports = mongoose.model("Lead", leadSchema);
