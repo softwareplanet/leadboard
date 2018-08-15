5a123afbc9cda317dd8a66efc0be77da93eb37ae
Vlad Kozlenko <vladislavko@softwareplanet.uk.com>
2018-08-15 16:01:51 +0300

    Fixed validation.

--- validation/lead.js
+++ validation/lead.js
@@ -1,5 +1,6 @@
-const Validator = require("validator");
-const isEmpty = require("lodash.isempty");
+import Validator from "validator";
+import isEmpty from "lodash.isempty";
+import { isNumber } from "./validationUtils";
 
 module.exports = function validateLeadInput(data) {
   let errors = {};
@@ -7,18 +8,16 @@ module.exports = function validateLeadInput(data) {
   if (isEmpty(data.owner)) errors.owner = "Owner ID cannot be empty";
   if (isEmpty(data.stage)) errors.stage = "Stage ID cannot be empty";
 
-  data.order = !isEmpty(data.order) ? data.order : "";
-  data.name = !isEmpty(data.name) ? data.name : "";
-
-  if (!Validator.isNumeric(data.order)) {
-    errors.order = "Order must be a number";
-  }
+  if (!isNumber(data.order)) errors.order = "Order must be a number";
 
   if (!Validator.isLength(data.name, { max: 30 })) {
     errors.name = "Lead name cannot be more 30 characters";
   }
 
-  //TODO: Check if owner, domain and stage are valid IDs
+  if (isEmpty("" + data.order)) errors.order = "Order cannot be empty";
+
+  if (isEmpty("" + data.name)) errors.name = "Name cannot be empty";
+
   return {
     errors,
     hasErrors: !isEmpty(errors)
