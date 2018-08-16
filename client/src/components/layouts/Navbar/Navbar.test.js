f0d62325baf5775ffec214e0896b89237605e0ef
Denys Kompaniiets <denyskom@interlink-ua.com>
2018-08-16 15:55:09 +0300

    add second test

--- client/src/components/layouts/Navbar/Navbar.test.js
+++ client/src/components/layouts/Navbar/Navbar.test.js
@@ -2,38 +2,46 @@ import React from 'react'
 import { configure, shallow, mount } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import { Navbar } from  './Navbar'
-import { MemoryRouter } from "react-router-dom";
+import styles from "./Navbar.css"
 
 configure({adapter: new Adapter});
 
 describe('<Navbar /> :', () => {
-    let wrapper;
-    let logoutUser;
+  let wrapper;
+  let logoutUser;
 
-    let auth = {
-      isAuthenticated: true,
-      userid: '5b6a9cca9a32282c3e8435c5',
-      domainid: '5b6a9cca9a32282c3e8435c4',
-      userName: 'John Doye',
-      domainName: 'Microsoft'
-    };
+  let auth = {
+    isAuthenticated: true,
+    userid: '5b6a9cca9a32282c3e8435c5',
+    domainid: '5b6a9cca9a32282c3e8435c4',
+    userName: 'John Doye',
+    domainName: 'Microsoft'
+  };
 
-    let location = {
-        pathname: "/home"
-    };
+  let location = {
+    pathname: "/home"
+  };
 
-    it('should call method logoutUser user when Logout clicked', () => {
-        logoutUser = jest.fn();
-        wrapper = shallow(<Navbar location={location} auth={auth} logoutUser={logoutUser} />);
-        wrapper.find('li')
-            .filterWhere(li => {
-                return li.find('div')
-                    .filterWhere(div =>
-                        div.prop('children').indexOf('Logout') !== -1).exists();
-            })
-            .forEach(li => {li.simulate('click')});
-        expect(logoutUser).toHaveBeenCalledTimes(1);
-    });
+  it('should call method logoutUser user when Logout clicked', () => {
+    logoutUser = jest.fn();
+    wrapper = shallow(<Navbar location={location} auth={auth} logoutUser={logoutUser}/>);
+    wrapper.find('li')
+      .filterWhere(li => {
+        return li.find('div')
+          .filterWhere(div =>
+            div.prop('children').indexOf('Logout') !== -1).exists();
+      })
+      .forEach(li => {
+        li.simulate('click')
+      });
+    expect(logoutUser).toHaveBeenCalledTimes(1);
+  });
 
+  it('should change background color if it on same path as linked to', () => {
+    wrapper = shallow(<Navbar location={location} auth={auth} logoutUser={logoutUser}/>);
+    console.log(wrapper.filter(`.${styles.active}`).exist());
 
+    expect(wrapper.filter(`.${styles.active}`)).toBeTruthy();
+  });
+  
 });
