0332d127760ec98128d506452fb7461c25381329
Yarik Mokhurenko <yaroslavmo@softwareplanet.uk.com>
2018-08-16 13:15:02 +0300

    finish tests

--- client/src/components/modal/AddLead/select_stage/SelectStageOnCreation.test.js
+++ client/src/components/modal/AddLead/select_stage/SelectStageOnCreation.test.js
@@ -1,37 +1,62 @@
 import "jsdom-global/register";
 import React from "react";
 import { mount, shallow } from "enzyme";
-import { expect } from "chai";
+
 import SelectStageOnCreation from "./SelectStageOnCreation";
-import store from "../../../../store";
 import styles from "./SelectStageOnCreation.css";
+import { wrap } from "module";
+import classNames from "classnames";
 
 describe("<SelectStageOnCreation />", () => {
   let wrapper;
-  let onStageChange = jest.fn();
+  const onStageChange = jest.fn();
+  let activateStageStyle;
   const stages = [{ _id: 1, name: "interest" }, { _id: 2, name: "promoting" }, { _id: 3, name: "action" }];
   beforeEach(() => {
     wrapper = mount(<SelectStageOnCreation stages={stages} onStageChange={onStageChange} />);
+    activateStageStyle = wrapper.instance().activateStageStyle;
   });
 
   it("renders without crashing", () => {
-    expect(wrapper).to.have.length(1);
+    expect(wrapper).toHaveLength(1);
   });
 
   it("shows proper amount of stages", () => {
     const stageClass = styles.radio;
-    console.log(wrapper.props().stages);
-    expect(wrapper.find(`.${stageClass}`).length).to.equal(wrapper.props().stages.length);
+    expect(wrapper.find(`.${stageClass}`).length).toEqual(wrapper.props().stages.length);
   });
 
   it("first stages active by default", () => {
     const stageClass = styles.radio;
-    console.log(wrapper.html());
     expect(
       wrapper
         .find(`.${stageClass}`)
         .at(0)
         .hasClass(styles.active)
-    ).equal(true);
+    ).toEqual(true);
+  });
+
+  it("click on stage trigger function that select the stage", () => {
+    wrapper
+      .find("[type='radio']")
+      .at(2)
+      .simulate("click", {});
+    expect(onStageChange).toHaveBeenCalled();
+  });
+
+  it("click on stage trigger function that add active class to stage", () => {
+    const stageClass = styles.radio;
+    wrapper
+      .find("[type='radio']")
+      .at(2)
+      .simulate("click", {});
+    wrapper.update();
+    expect(
+      wrapper
+        .find(`.${stageClass}`)
+        .at(2)
+        .render()
+        .hasClass(styles.active)
+    ).toEqual(true);
   });
 });
