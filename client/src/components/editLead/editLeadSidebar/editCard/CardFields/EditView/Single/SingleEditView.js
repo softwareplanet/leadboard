23c98ada475d520c49f75e40da728dccec8634c1
Valentyn Tulub <valentyntu@softwareplanet.uk.com>
2018-08-17 15:40:58 +0300

    remove binds from SingleEditView

--- client/src/components/editLead/editLeadSidebar/editCard/CardFields/EditView/Single/SingleEditView.js
+++ client/src/components/editLead/editLeadSidebar/editCard/CardFields/EditView/Single/SingleEditView.js
@@ -9,13 +9,13 @@ class SingleEditView extends Component {
     updatedValue: ""
   };
 
-  onChangeEditField(name, value) {
+  onChangeEditField = (name, value) => {
       this.setState({ updatedValue: value });
-  }
+  };
 
   onSaveClicked() {
     this.props.onChange(this.props.fieldName, this.state.updatedValue);
-  }
+  };
 
   componentWillMount() {
     this.setState({ updatedValue: this.props.fieldValue });
@@ -27,7 +27,7 @@ class SingleEditView extends Component {
         <EditFieldGroup
           name={this.props.fieldName}
           value={this.props.fieldValue}
-          onChange={this.onChangeEditField.bind(this)}
+          onChange={this.onChangeEditField}
         />
         <div className={styles.actions}>
           <button className={styles.cancelButton}>
@@ -47,7 +47,8 @@ class SingleEditView extends Component {
 
 SingleEditView.propTypes = {
   fieldName : PropTypes.string,
-  fieldValue : PropTypes.string
+  fieldValue : PropTypes.string,
+  onChange: PropTypes.func
 };
 
 export default SingleEditView;
