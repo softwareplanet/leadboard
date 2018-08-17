1a7deab6246780285661d6ece64878669d238877
Vlad Kozlenko <vladislavko@softwareplanet.uk.com>
2018-08-17 14:55:24 +0300

    formatting

--- mongoose.js
+++ mongoose.js
@@ -1,17 +1,17 @@
 import mongoose from "mongoose";
 
-const MONGODB_URL = 'mongodb://localhost/leadster' + (process.env.NODE_ENV === 'test'? '-test': '');
-console.log('Using: ' + MONGODB_URL);
+const MONGODB_URL = "mongodb://localhost/leadster" + (process.env.NODE_ENV === "test" ? "-test" : "");
+console.log("Using: " + MONGODB_URL);
 
-export const connectToMongoose = () =>{
+export const connectToMongoose = () => {
   mongoose.Promise = Promise;
-  const promise = mongoose.connect(MONGODB_URL, {useMongoClient: true});
+  const promise = mongoose.connect(MONGODB_URL, { useMongoClient: true });
 
   promise
     .then(function(db) {
-      db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+      db.on("error", console.error.bind(console, "MongoDB connection error:"));
     })
     .catch((err) => {
-      console.log('MongoDb connection error: ' + err)
+      console.log("MongoDb connection error: " + err);
     });
 };
