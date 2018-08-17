d742f96c8eea682493083dd734593cfbe8bf6794
Poshtarenko Nazar <nazarpo@softwareplanet.uk.com>
2018-08-17 16:46:52 +0300

    Merge branch 'master' of github.com:softwareplanet/leadboard into 19-add-lead-autocomplete
    # Conflicts:
    #	api/index.js

--- api/organization/index.js
+++ api/organization/index.js
@@ -1,5 +1,4 @@
 import { Router } from "express";
-import { require_auth } from "../authorize";
 import Organization from "../../models/organization";
 
 const router = new Router();
@@ -7,7 +6,7 @@ const router = new Router();
 // @route   GET api/organization
 // @desc    Return all organizations by domain
 // @access  Private
-router.get("/", require_auth, function(req, res) {
+router.get("/", function(req, res) {
   const domain = req.query.domain || req.body.domain;
   if (!domain) {
     return res.status(400).json({ errors: { domain: "Domain cannot be empty" } });
