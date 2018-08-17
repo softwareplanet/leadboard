74b08e8159b11e8441158b737d5addbc0091e468
SamardakOleh <olehsa@softwareplanet.uk.com>
2018-08-17 12:45:18 +0300

    prepare to deploy

--- app.js
+++ app.js
@@ -1,17 +1,32 @@
 import http from "http";
-var ip = "127.0.0.1";
-var port = 5000;
-
 import express from "./express";
 import api from "./api";
 
 var app = express(api);
 const server = http.createServer(app);
 
+var port = normalizePort(process.env.PORT || "5000");
+
 setImmediate(() => {
-  server.listen(port, ip, () => {
-    console.log("Express server listening on http://%s:%d", ip, port);
+  server.listen(port, () => {
+    console.log("Express server listening on port %s", port);
   });
 });
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
 
 export default app;
--- express.js
+++ express.js
@@ -1,19 +1,22 @@
-import express from 'express'
-import cors from 'cors'
-import compression from 'compression'
-import morgan from 'morgan'
-import bodyParser from 'body-parser'
-import mongoose from './mongoose'
+import express from "express";
+import cors from "cors";
+import compression from "compression";
+import morgan from "morgan";
+import bodyParser from "body-parser";
+import path from "path";
+import mongoose from "./mongoose";
 
-export default (routes) => {
-  const app = express()
+export default routes => {
+  const app = express();
 
-  app.use(cors())
-  app.use(compression())
-  app.use(morgan('dev'))
-  app.use(bodyParser.urlencoded({ extended: false }))
-  app.use(bodyParser.json())
-  app.use(routes)
+  app.use(cors());
+  app.use(compression());
+  app.use(morgan("dev"));
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(bodyParser.json());
+  app.use(routes);
 
-  return app
-}
+  if (process.env.PROD) app.use(express.static(path.join(__dirname, "/client/build")));
+
+  return app;
+};
