9090d7c1e9169c23bdaca6dd190838b9c1c1156c
Valentyn Tulub <valentyntu@softwareplanet.uk.com>
2018-08-21 17:28:04 +0300

    Merge remote-tracking branch 'origin/6-detail-leads-info' into 35-edit-leads-organisation

--- client/src/modules/lead/EditLead/EditLeadSidebar/EditCard/CardFields/CardField.js
+++ client/src/modules/lead/EditLead/EditLeadSidebar/EditCard/CardFields/CardField.js
@@ -1,18 +1,35 @@
-import React, { Component } from 'react';
-import styles from './CardField.css'
+import React, { Component } from "react";
+import styles from "./CardField.css";
+import { Link } from "react-router-dom";
+
 
 class CardField extends Component {
-    render() {
-        return (
-            <div className={styles.fieldValue}>
-                <div className={styles.fieldValueWrapper}>
-                    <div id="fieldLabel" className={styles.fieldLabel}>
-                        {this.props.fieldName}
-                        </div>
-                </div>
+  render() {
+    let addValue = (
+      <span className={styles.addValue}>
+        <Link to=' '>+ Add value</Link>
+      </span>
+    );
+    let value = (
+      <div id="fieldValue" className={styles.customFieldValueWrap}>
+        <span className={styles.customFieldValue}>
+          <Link to=' '>{this.props.customFieldValue}</Link>
+        </span>
+      </div>
+    );
+    return (
+      <div className={styles.customValue}>
+        <div className={styles.allCustomFieldsWrapper}>
+          <div className={styles.customFieldsWrapper}>
+            <div id="fieldLabel" className={styles.customFieldLabelWrap}>
+              <span className={styles.customFieldLabel}>{this.props.customFieldName}</span>
             </div>
-        )
-    }
+            {this.props.customFieldValue.length === 0 ? addValue : value}
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default  CardField;
+export default CardField;
