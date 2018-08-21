a6a13552e69a04aca256553889f718f85ffa23ec
SamardakOleh <olehsa@softwareplanet.uk.com>
2018-08-21 11:04:07 +0300

    create empty tabs. Add items to header with styles

--- client/src/modules/lead/EditLead/EditLeadSidebar/EditLeadSidebar.js
+++ client/src/modules/lead/EditLead/EditLeadSidebar/EditLeadSidebar.js
@@ -3,6 +3,6 @@ import styles from "./EditLeadSidebar.css";
 
 export default class EditLeadSidebar extends Component {
   render() {
-    return <div className={styles.sudebar} />;
+    return <div className={styles.sidebar} />;
   }
 }
--- client/src/modules/lead/EditLead/EditLeadTabs/EditLeadTabs.js
+++ client/src/modules/lead/EditLead/EditLeadTabs/EditLeadTabs.js
@@ -3,6 +3,19 @@ import styles from "./EditLeadTabs.css";
 
 export default class EditLeadTabs extends Component {
   render() {
-    return <div className={styles.activity} />;
+    return (
+      <div className={styles.tabs}>
+        <ul className={styles.header}>
+          <li className={styles.headerItem}>Take notes</li>
+          <li className={styles.headerItem}>Add activity</li>
+          <li className={styles.headerItem}></li>{/* Send email */}
+          <li className={styles.headerItem}></li>{/* Upload files */}
+          <li className={styles.headerItem}></li>{/* Close button */}
+        </ul>
+        <div className={styles.content}>
+          Footer
+        </div>
+      </div>
+    );
   }
 }
