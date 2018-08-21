f8c39f2455515bc30636315164ad5f5d50f97181
SamardakOleh <olehsa@softwareplanet.uk.com>
2018-08-21 09:34:19 +0300

    add validation to lead's name popover

--- client/src/modules/lead/EditLead/EditLeadHeader/EditLeadPopover/EditLeadPopover.js
+++ client/src/modules/lead/EditLead/EditLeadHeader/EditLeadPopover/EditLeadPopover.js
@@ -1,16 +1,24 @@
 import React, { Component } from "react";
-import { Popover, Card, CardHeader, CardBody, CardFooter } from "reactstrap";
+import { Popover, Card, CardBody, CardFooter } from "reactstrap";
 import styles from "./EditLeadPopover.css";
+import { flow, isEmpty, trim } from "lodash/fp";
 
 class EditLeadPopover extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      errors: {
+        value: ""
+      }
+    };
   }
 
   componentWillReceiveProps = (nextProps) => {
     this.setState({
-      name: nextProps.value
+      value: nextProps.value,
+      errors:{
+        value: ""
+      }
     })
   }
 
@@ -27,11 +35,14 @@ class EditLeadPopover extends Component {
           <div className={styles.header}>{this.props.title}</div>
           <CardBody className={styles.container}>
             <div className={styles.inputContainer}>
-              <input onChange={this.onChange} className={styles.input} defaultValue={this.props.value} />
+              <input 
+              onChange={this.onChange} 
+              className={this.isBlank(this.state.errors.value) ? styles.input : styles.invalidInput} 
+              defaultValue={this.props.value} />
             </div>
           </CardBody>
           <CardFooter className={styles.buttons}>
-            <button onClick={() => this.props.onSave(this.state.name)} className={styles.buttonSave}>
+            <button onClick={() => this.onSave()} className={styles.buttonSave}>
               Save
             </button>
             <button onClick={this.props.onCancel} className={styles.button}>
@@ -43,10 +54,28 @@ class EditLeadPopover extends Component {
     );
   }
 
+  isBlank = flow(
+    trim,
+    isEmpty
+  );
+
+  onSave = () => {
+    let { errors } = this.state;
+    if (this.isBlank(this.state.value)){
+      errors.value = "Value must be not empty"
+      this.setState(errors)
+    }
+    else {
+      errors.value = ""
+      this.setState(errors)
+      this.props.onSave(this.state.value)
+    }
+  }
+
   onChange = e => {
     this.setState({
       ...this.state,
-      name: e.target.value
+      value: e.target.value
     });
   };
 }
