d039b7d1dc71fb78b890608e561f17dff0af54f0
Valentyn Tulub <valentyntu@softwareplanet.uk.com>
2018-08-22 12:41:16 +0300

    add render props test for EditFieldGroup

--- client/src/modules/lead/EditLead/EditLeadSidebar/EditCard/CardFields/EditView/EditFieldGroup/EditFieldGroup.test.js
+++ client/src/modules/lead/EditLead/EditLeadSidebar/EditCard/CardFields/EditView/EditFieldGroup/EditFieldGroup.test.js
@@ -6,23 +6,34 @@ import { mount } from "enzyme";
 import { expect } from "chai";
 import sinon from "sinon";
 
+const customField = {
+  name: "Address",
+  value: "Grushevskoho st.",
+};
+
 describe("EditFieldGroup", () => {
   it("renders without crashing", function() {
     const fieldGroupWrapper = mount(<EditFieldGroup/>);
     expect(fieldGroupWrapper.find("input").hasClass(styles.input)).to.equal(true);
   });
 
+  it("renders props", function() {
+    const fieldGroupWrapper = mount(<EditFieldGroup
+      name={customField.name}
+      value={customField.value}
+    />);
+    expect(fieldGroupWrapper.find("span").text()).to.equal(customField.name);
+    expect(fieldGroupWrapper.find("input").props().defaultValue).to.equal(customField.value);
+  });
+
   it("reacts correctly on change", function() {
-    const name = "Address";
-    const value = "Grushevskoho st.";
-
-    let handleChange=sinon.spy();
-
-    const fieldGroupWrapper = mount(<EditFieldGroup name={name} value={value} onChange={handleChange}/>);
+    let handleChange = sinon.spy();
+    const fieldGroupWrapper = mount(<EditFieldGroup name={customField.name}
+                                                    value={customField.value}
+                                                    onChange={handleChange}/>);
     const input = fieldGroupWrapper.find("input");
     const newValue = "Gogolya st.";
     input.simulate("change", { target: { value: newValue } });
     expect(handleChange.calledOnce).to.equal(true);
   });
-
 });
