f76e48b7e1095a078647bbc4ffc1adcd9743a98d
Vlad Kozlenko <vladislavko@softwareplanet.uk.com>
2018-08-23 14:11:48 +0300

    Refactoring

--- client/src/modules/lead/EditLead/EditLeadContent/EditLeadContent.js
+++ client/src/modules/lead/EditLead/EditLeadContent/EditLeadContent.js
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
 import EditLeadTabs from "./EditLeadTabs/EditLeadTabs";
 import styles from "./EditLeadContent.css";
+import Notes from "../Notes/Notes";
 
 export default class EditLead extends Component {
-  
+
   render() {
     return (
       <div className={styles.content}>
-        <EditLeadTabs />
+        <EditLeadTabs/>
+        <Notes/>
       </div>
     );
   }
