59db39832088799916b41aa8c03d8bceea12157b
SamardakOleh <olehsa@softwareplanet.uk.com>
2018-08-23 13:40:19 +0300

    minor refactor

--- client/src/modules/lead/EditLead/EditLeadContent/EditLeadTabs/EditLeadEditor/EditLeadEditor.js
+++ client/src/modules/lead/EditLead/EditLeadContent/EditLeadTabs/EditLeadEditor/EditLeadEditor.js
@@ -27,10 +27,7 @@ export default class EditLeadEditor extends Component {
   }
 
   clearEditor = () => {
-    this.setState({
-      ...this.state,
-      noteText: ""
-    })
+    this.setState({ noteText: "" })
   }
 
   render() {
