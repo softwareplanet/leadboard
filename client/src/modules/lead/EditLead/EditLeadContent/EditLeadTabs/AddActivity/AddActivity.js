be7f69f722b5e767241943fa94bd201f2cf540fc
Denys Kompaniiets <denyskom@interlink-ua.com>
2018-08-28 19:09:00 +0300

    finish layouts

--- client/src/modules/lead/EditLead/EditLeadContent/EditLeadTabs/AddActivity/AddActivity.js
+++ client/src/modules/lead/EditLead/EditLeadContent/EditLeadTabs/AddActivity/AddActivity.js
@@ -29,7 +29,9 @@ export default class AddActivity extends Component {
     this.state = {
       activeTab: activityTypes[0].type,
       subject: "",
-      date:""
+      date:"",
+      time:"",
+      duration:"",
     }
   }
 
@@ -44,76 +46,99 @@ export default class AddActivity extends Component {
     this.setState({subject:event.target.value})
   };
 
-  onInputChange = event => {
-    this.setState({date:event.target.value})
+  onInputPick = (value, field) => {
+    this.setState({[field]: value})
   };
 
+  onDeleteClick = (e,field) => {
+    e.preventDefault();
+    this.setState({[field]:""})
+  };
+
+
+
   render() {
+    console.log(isBlank(this.state.duration) ? "" : `${this.state.duration.format("HH:mm")}`);
         return (
-      <form className={style.activityForm}>
-        <ActivityButtons
-          onButtonClick={this.onTypeButtonClick}
-          activeButton={this.state.activeTab}
-          buttons={activityTypes}
-          groupClassName={style.typeButtons}
-          buttonsClassName={style.typeButton}
-          activeClassName={style.typeButtonActive}
-          imgClassName={style.iconImg}
-        />
-        <input
-          onChange={this.onSubjectChange}
-          className={style.typeInput}
-          placeholder={this.state.activeTab}
-          value={this.state.subject}
-          type="text"/>
-        <div className={style.dateInputs}>
-          <label>
-            <span className={style.dateInputSpan}>DATE</span>
-            <div className={style.inputContainer}>
-              <DatePicker
-                placeholderText={`${moment().format("MM/DD/YYYY")}`}
-                className={style.dateInput}
-                popperPlacement="bottom"/>
-              <button className={style.inputButton}>
-                <img className={style.inputImg} src={deleteIcon} alt="del" />
-              </button>
-            </div>
-          </label>
-          <label>
-            <span className={style.dateInputSpan}>TIME</span>
-            <div className={style.inputContainer}>
-              <DatePicker
-                className={style.dateInput}
-                popperPlacement="bottom"
-                showTimeSelect
-                showTimeSelectOnly
-                timeIntervals={15}
-                timeCaption="Time" />
-              <button className={style.inputButton}>
-                <img className={style.inputImg} src={deleteIcon} alt="del" />
-              </button>
-            </div>
-          </label>
-          <label>
-            <span className={style.dateInputSpan}>DURATION</span>
-            <div className={style.inputContainer}>
-              <DatePicker
-                className={style.dateInput}
-                showTimeSelect
-                showTimeSelectOnly
-                timeIntervals={15}
-                minTime={moment().hours(0).minutes(15)}
-                maxTime={moment().hours(8).minutes(0)}
-                timeFormat="HH:mm"
+          <form className={style.activityForm}>
+            <div className={style.activityContainer}>
+              <ActivityButtons
+                onButtonClick={this.onTypeButtonClick}
+                activeButton={this.state.activeTab}
+                buttons={activityTypes}
+                groupClassName={style.typeButtons}
+                buttonsClassName={style.typeButton}
+                activeClassName={style.typeButtonActive}
+                imgClassName={style.iconImg}
               />
-              <button className={style.inputButton}>
-                <img className={style.inputImg} src={deleteIcon} alt="del" />
-              </button>
+              <input
+                onChange={this.onSubjectChange}
+                className={style.typeInput}
+                placeholder={this.state.activeTab}
+                value={this.state.subject}
+                type="text" />
+              <div className={style.dateInputs}>
+                <label>
+                  <span className={style.dateInputSpan}>DATE</span>
+                  <div className={style.inputContainer}>
+                    <DatePicker
+                      selected={this.state.date}
+                      onChange={(date) => this.onInputPick(date, "date")}
+                      placeholderText={`${moment().format("MM/DD/YYYY")}`}
+                      className={style.dateInput}
+                      popperPlacement="bottom" />
+                    <button onClick={(e) => this.onDeleteClick(e, "date")} className={style.inputButton}>
+                      <img className={style.inputImg} src={deleteIcon} alt="del" />
+                    </button>
+                  </div>
+                </label>
+                <label>
+                  <span className={style.dateInputSpan}>TIME</span>
+                  <div className={style.inputContainer}>
+                    <DatePicker
+                      selected={isBlank(this.state.time) ? "" : this.state.time}
+                      dateFormat="LT"
+                      timeFormat="HH:mm A"
+                      onChange={(time) => this.onInputPick(time, "time")}
+                      className={style.dateInput}
+                      popperPlacement="bottom"
+                      showTimeSelect
+                      showTimeSelectOnly
+                      timeIntervals={15}
+                      timeCaption="Time" />
+                    <button onClick={(e) => this.onDeleteClick(e, "time")} className={style.inputButton}>
+                      <img className={style.inputImg} src={deleteIcon} alt="del" />
+                    </button>
+                  </div>
+                </label>
+                <label>
+                  <span className={style.dateInputSpan}>DURATION</span>
+                  <div className={style.inputContainer}>
+                    <DatePicker
+                      className={style.dateInput}
+                      // customInput={<input value={isBlank(this.state.duration) ? "" : `${this.state.duration.format("HH:mm")}`}/>}
+                      selected={isBlank(this.state.duration) ? "" : this.state.duration}
+                      onChange={(duration) => this.onInputPick(duration, "duration")}
+                      dateFormat="LT"
+                      showTimeSelect
+                      showTimeSelectOnly
+                      timeIntervals={15}
+                      minTime={moment().hours(0).minutes(15)}
+                      maxTime={moment().hours(8).minutes(0)}
+                      timeFormat="HH:mm"
+                    />
+                    <button onClick={(e) => this.onDeleteClick(e, "duration")} className={style.inputButton}>
+                      <img className={style.inputImg} src={deleteIcon} alt="del" />
+                    </button>
+                  </div>
+                </label>
+              </div>
             </div>
-          </label>
-        </div>
-        <textarea className={style.note} placeholder="Notes"/>
-      </form>
+            <div className={style.footer}>
+              <button onClick={this.onCancel} className={style.button}><span>Cancel</span></button>
+              <button onClick={this.onSave} className={style.buttonSave}><span>Save</span></button>
+            </div>
+          </form>
     );
   }
 }
