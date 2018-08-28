e2d227d8f6d5b11cc452e2d956972734999d4032
Denys Kompaniiets <denyskom@interlink-ua.com>
2018-08-28 09:17:12 +0300

    remove unnecessary test

--- client/src/modules/layouts/Navbar/Navbar.test.js
+++ client/src/modules/layouts/Navbar/Navbar.test.js
@@ -14,8 +14,8 @@ describe("<Navbar /> :", () => {
     isAuthenticated: true,
     userid: "5b6a9cca9a32282c3e8435c5",
     domainid: "5b6a9cca9a32282c3e8435c4",
-    userName: "John Doye",
-    domainName: "Microsoft"
+    userName: "John Doe",
+    domainName: "Test domain"
   };
 
   let location = {
@@ -28,9 +28,4 @@ describe("<Navbar /> :", () => {
     wrapper.find("#logout").simulate("click");
     expect(logoutUser).toHaveBeenCalledTimes(1);
   });
-
-  it("should change class to active if current pass equal to elements path value ", () => {
-    wrapper = shallow(<Navbar auth={auth} location={location} logoutUser={logoutUser} />);
-    expect(wrapper.find("#deals").first().hasClass(styles.active)).toBeTruthy();
-  })
 });
