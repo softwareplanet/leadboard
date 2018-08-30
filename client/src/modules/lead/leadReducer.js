049429249ba150665aa7f13feece73811d5bb30e
Vlad Kozlenko <vladislavko@softwareplanet.uk.com>
2018-08-30 11:24:13 +0300

    remove warnings propTypes editLead

--- client/src/modules/lead/leadReducer.js
+++ client/src/modules/lead/leadReducer.js
@@ -5,7 +5,7 @@ import {
   UPDATE_ORGANIZATION,
 } from "./types";
 
-const initialState = [];
+const initialState = {};
 
 export default function(state = initialState, action) {
   switch (action.type) {
