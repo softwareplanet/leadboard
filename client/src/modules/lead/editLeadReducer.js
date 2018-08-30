0204f830d522a9e02982c51be664e4addf495e2d
Vlad Kozlenko <vladislavko@softwareplanet.uk.com>
2018-08-30 11:15:54 +0300

    refactoring

--- client/src/modules/lead/editLeadReducer.js
+++ client/src/modules/lead/editLeadReducer.js
@@ -1,8 +1,6 @@
-import activityReducer from "./EditLead/Activities/activityReducer";
 import leadReducer from "./leadReducer";
 import { combineReducers } from "redux";
 
 export default combineReducers({
   lead: leadReducer,
-  activities: activityReducer,
 });
