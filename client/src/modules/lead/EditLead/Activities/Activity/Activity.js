a773373b4f48df2327498f92a378919db77810e7
Vladyslav Tymofeiev <vladyslavty@interlink-ua.com>
2018-08-31 14:37:53 +0300

    add time checking function for different output of time styles

--- client/src/modules/lead/EditLead/Activities/Activity/Activity.js
+++ client/src/modules/lead/EditLead/Activities/Activity/Activity.js
@@ -1,43 +1,74 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import styles from "./Activity.css";
 import doneMark from "../../../../../assets/done-mark.svg"
-import { dateFormater } from "./dateFormatter";
+import {dateFormater} from "./dateFormatter";
+import classNames from "classnames/bind";
+
+let cx = classNames.bind(styles);
 
 class Activity extends Component {
 
-    changeStatus = e => {
-        e.preventDefault();
-        e.target.className = styles.markAsDone;
-    };
+  changeStatus = e => {
+    e.preventDefault();
 
-    render() {
-        return (
-            <div className={styles.activityContent}>
-               <div className={styles.wrapper}>
-                   <div className={styles.actionButtons}>
-                    </div>
-                    <h3>
-                    <div className={styles.mark} onClick={this.changeStatus}>
-                            <img className={this.props.activity.done ? styles.markAsDone : styles.markAsNotDone} src={doneMark} />
-                    </div>
-                    <span className={styles.activityWrapper}>{this.props.activity.subject}</span>
-                </h3>
-               </div>
-                <div className={styles.wrapper}>
-                <div className={styles.activityDetails}>
-                    <span>
+  };
+
+  checkTime = (date, hasStartTime, status) => {
+    let activityDate = new Date(date);
+    let activityDay = new Date(activityDate.toLocaleDateString());
+    let currentDate = new Date(new Date().toLocaleDateString());
+
+    if(status) {
+      return styles.defaultTime;
+    }
+    else {
+      if (currentDate.getTime() === activityDay.getTime()) {
+        if(hasStartTime){
+          if (activityDate.getTime() < new Date().getTime()) {
+          return styles.expiredTime;
+        }
+          return styles.defaultTime;
+        }
+        return styles.today;
+      }
+      if (currentDate.getTime() > activityDay.getTime()) {
+        return styles.expiredTime;
+      }
+    }
+  };
+
+  render() {
+    console.log(new Date().getTime());
+    return (
+      <div className={styles.activityContent}>
+        <div className={styles.wrapper}>
+          <div className={styles.actionButtons}>
+          </div>
+          <h3>
+            <div className={styles.mark} onClick={this.changeStatus}>
+              <img alt={"status"} className={cx({
+                markedAsDone: this.props.activity.done === true,
+                markedAsNotDone: this.props.activity.done === false
+              })} src={doneMark}/>
+            </div>
+            <span className={styles.activityWrapper}>{this.props.activity.subject}</span>
+          </h3>
+        </div>
+        <div className={styles.wrapper}>
+          <div className={styles.activityDetails}>
+                    <span className={this.checkTime(this.props.activity.date, this.props.activity.hasStartTime, this.props.activity.done)}>
                         {dateFormater(this.props.activity.date, this.props.activity.hasStartTime)}
                     </span>
-                    <span className={styles.separator}></span>
-                    <span className={styles.author}>{this.props.author}</span>
-                </div>
-                <div className={styles.relatedItems}>
+            <span className={styles.separator}></span>
+            <span className={styles.author}>{this.props.author}</span>
+          </div>
+          <div className={styles.relatedItems}>
 
-                </div>
-            </div>
-            </div>
-        )
-    }
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default Activity;
