9f9b4944c0898539c38b69e81c9adf85c0789291
Vladyslav Tymofeiev <vladyslavty@interlink-ua.com>
2018-08-31 16:55:34 +0300

    fix after review

--- models/activity.js
+++ models/activity.js
@@ -1,25 +1,25 @@
 import mongoose from "mongoose";
 
 const activitySchema = new mongoose.Schema({
-    type: {
-        type: String,
-        enum: ["Call", "Meeting", "Task", "Deadline", "Email", "Lunch"],
-        default: "Call"
-    },
-    subject: { type: String, default: "Call" },
-    date: { type: Date, default: Date.now },
-    hasStartTime: {type: Boolean, default:false},
-    duration: { type: Number },
-    note: { type: String, default:""},
-    assignedTo: { type: mongoose.Schema.Types.ObjectId, ref: "User"},
-    lead: { type: mongoose.Schema.Types.ObjectId, ref: "Lead"},
-    participants:  [{type: mongoose.Schema.Types.ObjectId, ref:"Contact"}],
-    organization: {type: mongoose.Schema.Types.ObjectId, ref: "Organization"},
-    done: {type: Boolean, default: false},
-    createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "User"},
-    modifiedBy: { type: mongoose.Schema.Types.ObjectId, ref: "User"},
-    createdAt: { type: Date, default: Date.now },
-    updatedAt: { type: Date, default: Date.now },
+  type: {
+    type: String,
+    enum: ["Call", "Meeting", "Task", "Deadline", "Email", "Lunch"],
+    default: "Call"
+  },
+  subject: {type: String, default: "Call"},
+  date: {type: Date, default: Date.now},
+  hasStartTime: {type: Boolean, default: false},
+  duration: {type: Number},
+  note: {type: String, default: ""},
+  assignedTo: {type: mongoose.Schema.Types.ObjectId, ref: "User"},
+  lead: {type: mongoose.Schema.Types.ObjectId, ref: "Lead"},
+  participants: [{type: mongoose.Schema.Types.ObjectId, ref: "Contact"}],
+  organization: {type: mongoose.Schema.Types.ObjectId, ref: "Organization"},
+  done: {type: Boolean, default: false},
+  createdBy: {type: mongoose.Schema.Types.ObjectId, ref: "User"},
+  modifiedBy: {type: mongoose.Schema.Types.ObjectId, ref: "User"},
+  createdAt: {type: Date, default: Date.now},
+  updatedAt: {type: Date, default: Date.now},
 });
 
 module.exports = mongoose.model("Activity", activitySchema);
