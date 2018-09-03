9cabcc8a19e60837345c2c9fc37ab8689fcb5637
Vladyslav Tymofeiev <vladyslavty@interlink-ua.com>
2018-09-03 17:07:33 +0300

    refactor date formatter using momentjs

--- client/src/modules/lead/EditLead/Activities/Activity/dateFormatter.js
+++ client/src/modules/lead/EditLead/Activities/Activity/dateFormatter.js
@@ -1,58 +1,35 @@
 import moment from "moment";
 
 export const dateFormatter = (date, hasStartTime) => {
-  let now = new Date();
   let activityDate = new Date(date);
 
-  if (moment(now).isSame(activityDate, "day")) {
-    return todayFormat(hasStartTime, activityDate);
+  if (hasStartTime) {
+    return moment(activityDate).calendar(null, {
+      lastDay: "[Yesterday at] h:mm A",
+      sameDay: "[Today at] h:mm A",
+      nextDay: "[Tomorrow at] h:mm A",
+      nextWeek: "dddd [at] h:mm A",
+      lastWeek: "[Last] dddd D [at] h:mm A",
+      sameElse: function (now) {
+        if (this.isSame(now, "year")) {
+          return "MMMM D h:mm A"
+        }
+        else return "MMMM D, YYYY hh:mm A"
+      }
+    });
+  } else {
+    return moment(activityDate).calendar(null, {
+      lastDay: "[Yesterday]",
+      sameDay: "[Today]",
+      nextDay: "[Tomorrow]",
+      lastWeek: "MMMM D",
+      nextWeek: "dddd",
+      sameElse: function (now) {
+        if (this.isSame(now, "year")) {
+          return "MMMM D"
+        }
+        else return "MMMM D, YYYY"
+      }
+    });
   }
-
-  let yesterday = getYesterday();
-  if (moment(activityDate).isSame(yesterday, "day")) {
-    return yesterdayFormat(hasStartTime, activityDate);
-  }
-
-  let tomorrow = getTomorrow();
-  if (moment(activityDate).isSame(tomorrow, "day")) {
-    return tomorrowFormat(hasStartTime, activityDate);
-  }
-
-  if (moment(now).isSame(activityDate, "year")) {
-    return thisYearFormat(hasStartTime, activityDate);
-  }
-
-  return notThisYearFormat(hasStartTime, activityDate);
-};
-
-const yesterdayFormat = (hasStartTime, date) => {
-  return hasStartTime ? `Yesterday at ${moment(date).format("hh:mm A")} ` : `Yesterday`;
-};
-
-const tomorrowFormat = (hasStartTime, date) => {
-  return hasStartTime ? `Tomorrow at ${moment(date).format("hh:mm A")} ` : `Tomorrow`;
-};
-
-const thisYearFormat = (hasStartTime, date) => {
-  return hasStartTime ? `${moment(date).format("MMMM DD hh:mm A")}` : ` ${ moment(date).format("MMMM DD")}`;
-};
-
-const todayFormat = (hasStartTime, date) => {
-  return hasStartTime ? `Today at  ${moment(date).format("hh:mm A")}` : `Today`;
-};
-
-const notThisYearFormat = (hasStartTime, date) => {
-  return hasStartTime ? `${moment(date).format("MMMM DD, YYYY hh:mm A")}` : `${moment(date).format("MMMM DD, YYYY")}`;
-};
-
-const getYesterday = () => {
-  let yesterday = new Date(new Date().toLocaleDateString());
-  yesterday.setDate(yesterday.getDate() - 1);
-  return yesterday;
-};
-
-const getTomorrow = () => {
-  let tomorrow = new Date(new Date().toLocaleDateString());
-  tomorrow.setDate(tomorrow.getDate() + 1);
-  return tomorrow;
 };
