861b0b1cf07f3148792ba05c7412ce048abcfe8f
Vlad Kozlenko <vladislavko@softwareplanet.uk.com>
2018-09-03 17:18:52 +0300

    add rest test on get all activities, mark activity as done for activity

--- api/activity/index.test.js
+++ api/activity/index.test.js
@@ -2,18 +2,32 @@ import request from "supertest";
 
 import express from "../../express";
 import routes from "..";
-import { dropTables, createUserAndDomain } from "../../test/db-prepare";
+import { dropTables, createUserAndDomain, createActivity, createLead } from "../../test/db-prepare";
 
 const app = () => express(routes);
 
 let cred;
+let activity;
+let lead;
 beforeEach(async done => {
   await dropTables();
   cred = await createUserAndDomain(app);
+  activity = await createActivity(app,cred.token,"Call", "First test call", 30);
+  activity = await createActivity(app,cred.token,"Call", "Second test call", 30);
+  lead = await createLead(app, cred.token, cred.userId, "id", 2, "Lead A");
+  console.log("lead  :" + lead);
   done();
 });
 
 describe("Activity", () => {
+  it("should return all lead's activities ", async () => {
+    const { body } = await request(app())
+      .get(`api/lead/${lead._id}/activities`)
+      .set("Authorization", cred.token)
+      .send();
+    expect(body.length).toEqual(2);
+  });
+
   it("should create a new activity", async () => {
     const { status, body } = await request(app())
       .post("/api/activity")
@@ -41,8 +55,17 @@ describe("Activity", () => {
       errors: {
         type: "Type cannot be empty",
         subject: "Subject cannot be empty",
-        duration: "Duration must be a number and cannot be empty"
-      }
+        duration: "Duration must be a number and cannot be empty",
+      },
     });
+  });
+
+  it("should mark activity as done", async () => {
+    activity.done = true;
+    const { body } = await request(app())
+      .patch("/api/activity")
+      .set("Authorization", cred.token)
+      .send({ activity });
+    expect(body.done).toEqual(true)
   });
 });
