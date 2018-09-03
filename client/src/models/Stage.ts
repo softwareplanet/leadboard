a825fc4a434c715d938a25ca2ef332b99dd0fc09
SamardakOleh <olehsa@softwareplanet.uk.com>
2018-09-03 16:04:00 +0300

    add optional type to stage

--- client/src/models/Stage.ts
+++ client/src/models/Stage.ts
@@ -2,7 +2,7 @@ import Funnel from "./Funnel";
 
 export default class Stage {
   public _id: string;
-  public funnel: Funnel;
+  public funnel: string | Funnel;
   public name: string;
   public order: number;
   public timestamp: Date;
