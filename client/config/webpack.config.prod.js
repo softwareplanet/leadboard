99b3705436ce5b92fce38c9067fbdf60bad91cdd
SamardakOleh <olehsa@softwareplanet.uk.com>
2018-09-03 12:51:06 +0300

    fix css modules

--- client/config/webpack.config.prod.js
+++ client/config/webpack.config.prod.js
@@ -197,9 +197,10 @@ module.exports = {
                     {
                       loader: require.resolve('css-loader'),
                       options: {
-                          importLoaders: 1,
-                          modules: true,
-                          localIdentName: "[name]__[local]___[hash:base64:5]"
+                        importLoaders: 1,
+                        modules: true,
+                        minimize: true,
+                        sourceMap: shouldUseSourceMap,
                       },
                     },
                     {
