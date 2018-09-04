037a62b870d8a52f9aee6699205b6e51d17f7ccd
Vladyslav Tymofeiev <vladyslavty@interlink-ua.com>
2018-09-04 15:16:45 +0300

    add ts models

--- client/src/models/User.ts
+++ client/src/models/User.ts
@@ -1,3 +1,12 @@
-export default interface User {
+import Domain from './Domain';
 
+export default interface User {
+  _id: string,
+  firstname: string,
+  lastname: string,
+  email: string,
+  password: string,
+  domain: Domain,
+  timestamp: Date,
+  avatar: string
 }
