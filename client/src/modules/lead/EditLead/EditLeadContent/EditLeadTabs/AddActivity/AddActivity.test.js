68a7f5ecd995d5f47b144ffad7954176495df3bf
Valentyn Tulub <valentyntu@softwareplanet.uk.com>
2018-09-05 16:29:10 +0300

    add unit tests for AddActivity

--- client/src/modules/lead/EditLead/EditLeadContent/EditLeadTabs/AddActivity/AddActivity.test.js
+++ client/src/modules/lead/EditLead/EditLeadContent/EditLeadTabs/AddActivity/AddActivity.test.js
@@ -50,26 +50,26 @@ describe("<AddActivity />", () => {
       });
   });
 
-  it("it should load correct state on edit", () => {
-    const activity = {
-      _id: "5b895a431270f10f86eb26b8",
-      duration: 60,
-      lead: "5b893533b1e18b6782dbab4f",
-      assignedTo: "5b7adf5182b8de5b65fc1b8c",
-      createdBy: "5b7adf5182b8de5b65fc1b8c",
-      __v: 0,
-      lastEditor: "5b7adf5182b8de5b65fc1b8c",
-      updatedAt: "2018-09-04T13:16:33.116Z",
-      createdAt: "2018-08-31T15:09:55.797Z",
-      done: false,
-      participants: [],
-      note: "",
-      hasStartTime: true,
-      date: "2018-09-09T21:30:00.000Z",
-      subject: "Meet Alice",
-      type: "Meeting",
-    };
+  const activity = {
+    _id: "5b895a431270f10f86eb26b8",
+    duration: 60,
+    lead: "5b893533b1e18b6782dbab4f",
+    assignedTo: "5b7adf5182b8de5b65fc1b8c",
+    createdBy: "5b7adf5182b8de5b65fc1b8c",
+    __v: 0,
+    lastEditor: "5b7adf5182b8de5b65fc1b8c",
+    updatedAt: "2018-09-04T13:16:33.116Z",
+    createdAt: "2018-08-31T15:09:55.797Z",
+    done: false,
+    participants: [],
+    note: "",
+    hasStartTime: true,
+    date: "2018-09-09T21:30:00.000Z",
+    subject: "Meet Alice",
+    type: "Meeting",
+  };
 
+  it("it should load correct state on edit", () => {
     let date = moment(activity.date);
     let dateStart = moment(date.startOf("day")._d);
     let time = moment.duration(moment(date._i).diff(dateStart)).asMinutes();
@@ -83,5 +83,27 @@ describe("<AddActivity />", () => {
 
     const wrapper = shallow(<AddActivity onSave={onActivitySaveMock} onCancel={() => jest.fn()} activity={activity} />);
     expect(wrapper.state()).toMatchObject(expectedState);
+  });
+
+  it("it should return updated activity on save", () => {
+    const wrapper = shallow(<AddActivity onSave={onActivitySaveMock} onCancel={() => jest.fn()} activity={activity} />);
+
+    const newSubject = "Visit Jackson";
+    wrapper.instance().onSubjectChange({ target: { value: newSubject } });
+
+    const newType = "Meeting";
+    wrapper.instance().onTypeButtonClick(newType);
+
+    const newDate = new Date();
+    wrapper.instance().onInputPick(newDate, "date");
+    wrapper.instance().onInputPick(moment(newDate).add(60, "minutes")._d, "time");
+
+    wrapper.find("#saveActivityButton").simulate("click");
+    expect(onActivitySaveMock.mock.calls[0][0])
+      .toMatchObject({
+        type: newType,
+        subject: newSubject,
+        date: newDate,
+      });
   });
 });
