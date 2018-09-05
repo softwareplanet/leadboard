7ba4d75954a5e058f2719f132682a6dae716c67b
Vlad Kozlenko <vladislavko@softwareplanet.uk.com>
2018-09-05 17:58:07 +0300

    add People.css.d.ts

--- client/src/modules/layouts/Contacts/People/People.tsx
+++ client/src/modules/layouts/Contacts/People/People.tsx
@@ -2,9 +2,33 @@ import * as React from 'react';
 import { connect } from 'react-redux';
 import NavBar from '../../Navbar/Navbar';
 import { loadContacts } from '../../../common/autocomplete/contact/contactActions';
+import Contact from '../../../../models/Contact';
+import Table from '../../../../modules/common/Table/Table';
+import * as styles from './People.css';
+
 interface Props {
+  contacts:Contact[];
   loadContacts(): object;
 }
+
+const columns = [
+  {
+    dataField: 'name',
+    text: 'Name',
+  },
+  {
+    dataField: 'organization.name',
+    text: 'Organization',
+  },
+  {
+    dataField: 'custom[0].value',
+    text: 'Phone',
+  },
+  {
+    dataField: 'custom[1].value',
+    text: 'Email',
+  },
+];
 
 class People extends React.Component<Props, object> {
   public componentWillMount = () => {
@@ -15,6 +39,11 @@ class People extends React.Component<Props, object> {
     return (
       <div>
         <NavBar />
+        <p className={styles.peopleCounter}>{this.props.contacts.length} people</p>
+        <Table
+          contacts={this.props.contacts}
+          columns={columns}
+        />
       </div>
     );
   }
@@ -24,5 +53,4 @@ const mapStateToProps = (state: any) => ({
   contacts: state.contacts,
 });
 
-export { People };
 export default connect(mapStateToProps, { loadContacts })(People);
