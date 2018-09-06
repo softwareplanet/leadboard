43c2b4511aee0dd4f71c0ff1169f641e195c07b0
Vlad Kozlenko <vladislavko@softwareplanet.uk.com>
2018-09-06 14:43:55 +0300

    add ContactsDropDown component instead of DropDown in NavBar.tsx

--- client/src/modules/layouts/Navbar/Navbar.tsx
+++ client/src/modules/layouts/Navbar/Navbar.tsx
@@ -1,36 +1,21 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
 import { Link, NavLink, RouteComponentProps, withRouter } from 'react-router-dom';
-import ReactSVG from 'react-svg';
-import { Dropdown, DropdownMenu, DropdownToggle } from 'reactstrap';
-import contactIcon from '../../../assets/contacts-icon.svg';
 import dealsIconActive from '../../../assets/deals-icon-active.svg';
 import dealsIcon from '../../../assets/deals-icon.svg';
 import profileIcon from '../../../assets/header-profile.svg';
 import { logoutUser } from '../../auth/authActions';
+import ContactsDropDown from './ContactsDropDown/ContactsDropDown';
 import * as styles from './Navbar.css';
 
 const leadsRoute = '/home';
-const peopleRoute = '/people';
 
 interface Props extends RouteComponentProps<any> {
   auth: any;
   logoutUser(history: any): void;
 }
 
-interface State {
-  isDropdownOpen: boolean;
-}
-
-class Navbar extends React.Component<Props, State> {
-  public state: State = {
-    isDropdownOpen: false,
-  };
-
-  public toggle = () => {
-    this.setState({ isDropdownOpen: !this.state.isDropdownOpen });
-  };
-
+class Navbar extends React.Component<Props, object> {
   public renderUserAvatar = () => {
     return this.props.auth && this.props.auth.avatar ?
       <img className={styles.userImg} src={this.props.auth.avatar} alt="user" /> :
@@ -60,30 +45,7 @@ class Navbar extends React.Component<Props, State> {
                    src={this.getDealsIcon()} alt="leads" />Leads
             </div>
           </NavLink>
-          <div className={this.props.location.pathname === '/people' ? styles.activeContacts : undefined}>
-            <Dropdown
-              className={this.state.isDropdownOpen ? styles.openedDropDown : styles.closedDropDown}
-              isOpen={this.state.isDropdownOpen}
-              toggle={this.toggle}
-            >
-              <DropdownToggle
-                className={styles.dropDownToggle}
-                onClick={this.toggle}
-                data-toggle="dropdown"
-                aria-expanded={this.state.isDropdownOpen}
-              >
-                <span><ReactSVG className={styles.contactIcon} src={contactIcon} /></span>
-                <span className={styles.linkText}>Contacts</span>
-              </DropdownToggle>
-              <DropdownMenu className={styles.dropDownMenu}>
-                <NavLink onClick={this.toggle} to={peopleRoute}>
-                  <div className={styles.menuItemDataWrapper}>
-                    <ReactSVG className={styles.peopleIcon} src={contactIcon} />People
-                  </div>
-                </NavLink>
-              </DropdownMenu>
-            </Dropdown>
-          </div>
+          <ContactsDropDown />
           <li className={styles.rightItem}>
             <div>
               {this.renderUserAvatar()}
