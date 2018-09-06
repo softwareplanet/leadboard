17ad7834e75b003889dbbcc9fd3f0c305a402321
Valentyn Tulub <valentyntu@softwareplanet.uk.com>
2018-09-06 11:17:19 +0300

    add backend will now try all unresolved routes as index.html route

--- express.js
+++ express.js
@@ -23,6 +23,9 @@ export default (routes) => {
 
   if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "/client/build")));
+    app.get('*', (req,res) =>{
+      res.sendFile(path.join(__dirname+'/client/build/index.html'));
+    });
     app.use(morgan("common"));
   } else {
     app.use(morgan("dev"));
