7d24f8fc0098b0efe7a4f03d6d81b0cfe7ec186e
Vladyslav Tymofeiev <vladyslavty@interlink-ua.com>
2018-09-07 13:04:03 +0300

    add constant activity status value to test

--- client/src/modules/lead/Dashboard/Lead/Lead.test.js
+++ client/src/modules/lead/Dashboard/Lead/Lead.test.js
@@ -2,6 +2,7 @@ import React from "react";
 import { configure, shallow } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import Lead from "./Lead";
+import { NoActivity} from "../activityStatuses";
 
 configure({ adapter: new Adapter });
 
@@ -22,13 +23,13 @@ describe("<Lead /> :", () => {
   let link = `/funnel/5b6bd8112e46b819428f9b52/lead/5b6c598084412364d01a73b4`;
 
   beforeEach(() => {
-    wrapper = shallow(<Lead lead={lead} link={link} activityStatus="NoActivity" />);
+    wrapper = shallow(<Lead lead={lead} link={link} activityStatus={NoActivity} />);
   });
 
   it("should render organization name if contact person is absent", () => {
     let leadWithoutContact = { ...lead };
     delete leadWithoutContact.contact;
-    wrapper = shallow(<Lead link={link} lead={leadWithoutContact} activityStatus="NoActivity" />);
+    wrapper = shallow(<Lead link={link} lead={leadWithoutContact} activityStatus={NoActivity} />);
     expect(wrapper.find("small").text()).toEqual(leadWithoutContact.organization.name);
   });
 
