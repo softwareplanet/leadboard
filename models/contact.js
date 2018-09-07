9df16bbba0f068a9ea27258d4b61fc7a9672d747
Vlad Kozlenko <vladislavko@softwareplanet.uk.com>
2018-09-07 12:28:52 +0300

    add owner field to contact and organization model

--- models/contact.js
+++ models/contact.js
@@ -1,20 +1,21 @@
 import mongoose from "mongoose";
-import { CONTACT, ORGANIZATION } from "./refs";
+import { CONTACT, ORGANIZATION, USER } from "./refs";
 
 // Person
 const contactSchema = new mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
   name: String,
-  organization: { type: mongoose.Schema.Types.ObjectId, ref: ORGANIZATION},
+  organization: { type: mongoose.Schema.Types.ObjectId, ref: ORGANIZATION },
   custom: [{ name: "string", value: "string" }],
+  owner: { type: mongoose.Schema.Types.ObjectId, required: true, ref: USER },
   domain: { type: mongoose.Schema.Types.ObjectId, required: true },
-  timestamp: { type: Date, default: Date.now }
+  timestamp: { type: Date, default: Date.now },
 });
 
 //custom fields
 const DEFAULT_CUSTOM_FIELDS = [{ name: "Phone", value: "" }, { name: "Email", value: "" }];
 
-contactSchema.pre("save", function (next) {
+contactSchema.pre("save", function(next) {
   if (this.isNew) {
     if (this.custom.length === 0) {
       this.custom = DEFAULT_CUSTOM_FIELDS;
--- models/organization.js
+++ models/organization.js
@@ -1,19 +1,20 @@
 import mongoose from "mongoose";
-import { ORGANIZATION } from "./refs";
+import { ORGANIZATION, USER } from "./refs";
 
 // Organization
 const organizationSchema = new mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
   name: String,
   domain: { type: mongoose.Schema.Types.ObjectId, required: true },
+  owner: { type: mongoose.Schema.Types.ObjectId, required: true, ref: USER },
   custom: [{ name: "string", value: "string" }],
-  timestamp: { type: Date, default: Date.now }
+  timestamp: { type: Date, default: Date.now },
 });
 
 //custom fields
 const DEFAULT_CUSTOM_FIELDS = [{ name: "Address", value: "" }];
 
-organizationSchema.pre("save", function (next) {
+organizationSchema.pre("save", function(next) {
   if (this.isNew) {
     if (this.custom.length === 0) {
       this.custom = DEFAULT_CUSTOM_FIELDS;
