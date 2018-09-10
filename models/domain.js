f2f4b800ba4bb21b8a2b54e355bdd5ba468f1cd8
Valentyn Tulub <valentyntu@softwareplanet.uk.com>
2018-09-10 16:47:19 +0300

    add new custom fields structure

--- models/domain.js
+++ models/domain.js
@@ -5,7 +5,38 @@ const domainSchema = new mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
   name: { type: String, required: [true, "Company name is required"] },
   timestamp: { type: Date, default: Date.now },
-  timezone: {type: String, default: "Etc/UTC" }
+  settings: {
+    customFields: {
+      lead: [{
+        key: { type: String, required: [true] },
+        name: { type: String, required: [true] },
+        label: String,
+        type: { type: String, required: [true] },
+        isAlwaysVisible: { type: Boolean, required: [true, true] },
+        isShownInAddDialog: { type: Boolean, required: [true, false] },
+        isDefault: { type: Boolean, required: [true, false] },
+      }],
+      contact: [{
+        key: { type: String, required: [true] },
+        name: { type: String, required: [true] },
+        label: String,
+        type: { type: String, required: [true] },
+        isAlwaysVisible: { type: Boolean, required: [true, true] },
+        isShownInAddDialog: { type: Boolean, required: [true, false] },
+        isDefault: { type: Boolean, required: [true, false] },
+      }],
+      organization: [{
+        key: { type: String, required: [true] },
+        name: { type: String, required: [true] },
+        label: String,
+        type: { type: String, required: [true] },
+        isAlwaysVisible: { type: Boolean, required: [true, true] },
+        isShownInAddDialog: { type: Boolean, required: [true, false] },
+        isDefault: { type: Boolean, required: [true, false] },
+      }],
+    },
+    timezone: { type: String, default: "Etc/UTC" },
+  },
 });
 
 export default mongoose.model(DOMAIN, domainSchema);
