38e652363252f5f1b7fcb3ea5de6ec619ba3de8e
Valentyn Tulub <valentyntu@softwareplanet.uk.com>
2018-09-11 12:39:25 +0300

    add domain default settings and timezone patch test

--- api/domain/index.test.js
+++ api/domain/index.test.js
@@ -2,6 +2,7 @@ import express from "../../express";
 import routes from "..";
 import { createUserAndDomain, dropTables } from "../../test/db-prepare";
 import Domain from "../../models/domain";
+import request from "supertest";
 
 const app = () => express(routes);
 
@@ -17,39 +18,67 @@ describe("Contact", () => {
     const domain = await Domain.findById(cred.domainId);
 
     const expectedSettings = {
-      customFields: {
-        lead: [],
-        contact: [
-          {
-            key: "phone",
-            name: "Phone",
-            type: "string",
-            isAlwaysVisible: true,
-            isShownInAddDialog: false,
-            isDefault: true,
-          },
-          {
-            key: "email",
-            name: "Email",
-            type: "string",
-            isAlwaysVisible: true,
-            isShownInAddDialog: false,
-            isDefault: true,
-          },
-        ],
-        organization: [
-          {
-            key: "address",
-            name: "Address",
-            type: "string",
-            isAlwaysVisible: true,
-            isShownInAddDialog: false,
-            isDefault: true,
-          },
-        ],
-      },
+      customFields: [
+        {
+          model: "Contact",
+          name: "Phone",
+          type: "string",
+          isAlwaysVisible: true,
+          isShownInAddDialog: false,
+          isDefault: true,
+        },
+        {
+          model: "Contact",
+          name: "Email",
+          type: "string",
+          isAlwaysVisible: true,
+          isShownInAddDialog: false,
+          isDefault: true,
+        },
+        {
+          model: "Organization",
+          name: "Address",
+          type: "string",
+          isAlwaysVisible: true,
+          isShownInAddDialog: false,
+          isDefault: true,
+        },
+      ],
     };
 
-    expect(domain.settings).toMatchObject(expectedSettings);
+    expect(domain.settings.customFields).toHaveLength(expectedSettings.customFields.length);
+  });
+
+  it("should fail to update domain's settings with incorrect custom fields' settings", async () => {
+    const newSettings = {
+      customFields: {},
+    };
+
+    const { status, body } = await request(app())
+      .patch(`/api/domain/${cred.domainId}/settings/`)
+      .set("Authorization", cred.token)
+      .send(newSettings);
+
+    const expectedErrors = {
+      errors: {
+        customFields: "You have to use POST|PATCH|DELETE api/domain/:domainId/settings/customFields/:customFieldId to modify custom fields",
+      },
+    };
+    expect(status).toBe(400);
+    expect(body).toMatchObject(expectedErrors);
+  });
+
+  it("should update domain's timezone", async () => {
+    const newSettings = {
+      timezone: "America/Los_Angeles",
+    };
+
+    const { status, body } = await request(app())
+      .patch(`/api/domain/${cred.domainId}/settings/`)
+      .set("Authorization", cred.token)
+      .send(newSettings);
+
+    expect(status).toBe(200);
+    expect(body.settings).toMatchObject(newSettings);
   });
 });
