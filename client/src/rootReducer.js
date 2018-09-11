1fa8576e4b11e0c332559112ed6cb6bf1f8599fe
Vlad Kozlenko <vladislavko@softwareplanet.uk.com>
2018-09-11 14:14:44 +0300

    create actions and reducer for domain settings

--- client/src/rootReducer.js
+++ client/src/rootReducer.js
@@ -5,6 +5,7 @@ import leadReducersAggregator from "./modules/lead/leadRootReducer";
 import { GET_ERRORS, CLEAR_STORE } from "./actionTypes";
 import contactReducer from "./modules/common/autocomplete/contact/contactReducer";
 import organizationReducer from "./modules/common/autocomplete/organization/organizationReducer";
+import settingsReducer from "./modules/lead/Dashboard/settingsReducer";
 
 const initialState = {};
 
@@ -23,6 +24,7 @@ const appReducer = combineReducers({
   errors: errorReducer,
   contacts: contactReducer,
   organizations: organizationReducer,
+  settings: settingsReducer,
 });
 
 export default function rootReducer(state, action) {
