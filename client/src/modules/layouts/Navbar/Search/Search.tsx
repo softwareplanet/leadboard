2a1eca5f8885345bf44b06ef9fe38e5e062fcc68
Poshtarenko Nazar <nazarpo@softwareplanet.uk.com>
2018-09-11 11:42:19 +0300

    replace common input by DateSearch component and add to this component input styles

--- client/src/modules/layouts/Navbar/Search/Search.tsx
+++ client/src/modules/layouts/Navbar/Search/Search.tsx
@@ -1,3 +1,7 @@
+import {
+  DataSearch,
+  ReactiveBase,
+} from '@appbaseio/reactivesearch';
 import * as React from 'react';
 import ReactSVG from 'react-svg';
 import searchIcon from '../../../../assets/search-icon.svg';
@@ -7,8 +11,21 @@ class Search extends React.Component {
   public render() {
     return (
       <div className={styles.searchWrapper}>
-        <span><ReactSVG className={styles.searchIcon} src={searchIcon} /></span>
-        <input type="text" className={styles.searchInput} placeholder="Search" />
+        <ReactiveBase
+          app="good-books-ds"
+          credentials="nY6NNTZZ6:27b76b9f-18ea-456c-bc5e-3a5263ebc63d"
+        >
+          <ReactSVG className={styles.searchIcon} src={searchIcon} />
+          <DataSearch
+            componentId={'search'}
+            placeholder={'Search'}
+            dataField={['original_title', 'original_title.search']}
+            debounce={100}
+            showIcon={false}
+            className={styles.search}
+            innerClass={{ input: styles.searchInput }}
+          />
+        </ReactiveBase>
       </div>
     );
   }
