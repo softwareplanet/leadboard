c8888c03127739a350541146df6eb8359246db64
Valentyn Tulub <valentyntu@softwareplanet.uk.com>
2018-09-11 17:25:48 +0300

    fix import in CustomFieldView

--- client/src/modules/lead/EditLead/EditLeadSidebar/EditCard/CardFields/EditView/CustomFieldView/CustomFieldView.tsx
+++ client/src/modules/lead/EditLead/EditLeadSidebar/EditCard/CardFields/EditView/CustomFieldView/CustomFieldView.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import CustomFieldSetting from '../../../../../../../../models/CustomFieldSetting';
+import CustomFieldSetting from '../../../../../../../../models/customFields/CustomFieldSetting';
 import * as styles from './CustomFieldView.css';
 
 interface Props{
