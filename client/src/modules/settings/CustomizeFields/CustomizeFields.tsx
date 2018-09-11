c553f4b6c4b31733eb00ef1963e3f9fc3f804b82
SamardakOleh <olehsa@softwareplanet.uk.com>
2018-09-11 12:28:07 +0300

    generate basic components

---
+++ client/src/modules/settings/CustomizeFields/CustomizeFields.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react'
+
+export default class CustomizeFields extends React.Component {
+  public render() {
+    return (
+      <div>CustomizeFields</div>
+    )
+  }
+}
---
+++ client/src/modules/settings/Pipelines/Pipelines.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react'
+
+export default class Pipelines extends React.Component {
+  public render() {
+    return (
+      <div>Pipelines</div>
+    )
+  }
+}
