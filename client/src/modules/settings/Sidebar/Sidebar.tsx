0662f5fa909cf9f417b275d36de7ea7db0909da7
SamardakOleh <olehsa@softwareplanet.uk.com>
2018-09-11 12:10:36 +0300

    add sidebar item styles

--- client/src/modules/settings/Sidebar/Sidebar.tsx
+++ client/src/modules/settings/Sidebar/Sidebar.tsx
@@ -1,11 +1,45 @@
 import * as React from 'react'
 import * as styles from './Sidebar.css'
 
-export default class Sidebar extends React.Component {
+interface State {
+  activeIndex: number
+}
+
+export default class Sidebar extends React.Component<object, State> {
+  public state: State = {
+    activeIndex: 0,
+  }
+  private settingsElements = ['Pipelines', 'Customize fields'];
 
   public render() {
     return (
-      <aside className={styles.sidebar}>'Aside'</aside>
+      <aside className={styles.sidebar}>
+        {this.getSettingsItems()}
+      </aside>
+    )
+  }
+
+  private select = (index: number) => {
+    this.setState({
+      activeIndex: index,
+    })
+  }
+
+  private getSettingsItems = () => {
+    return (
+      <ul>
+        { this.settingsElements.map((item, index) => 
+          ( 
+            <li 
+              key={index}
+              className={this.state.activeIndex === index ? styles.itemActive : styles.item}
+              onClick={() => this.select(index)}
+            >
+              { item }
+            </li>
+          )
+        ) }
+      </ul>
     )
   }
 }
