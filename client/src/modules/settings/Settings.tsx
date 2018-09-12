56597bc10adbb14430a448de962a6c1755edb651
SamardakOleh <olehsa@softwareplanet.uk.com>
2018-09-12 13:55:12 +0300

    minor code refactor

--- client/src/modules/settings/Settings.tsx
+++ client/src/modules/settings/Settings.tsx
@@ -1,8 +1,8 @@
-import * as React from 'react'
+import * as React from 'react';
 import { Route } from 'react-router-dom';
 import CustomizeFields from './CustomizeFields/CustomizeFields';
 import Pipelines from './Pipelines/Pipelines';
-import * as styles from './Settings.css'
+import * as styles from './Settings.css';
 import Sidebar from './Sidebar/Sidebar';
 
 export default class Settings extends React.Component {
