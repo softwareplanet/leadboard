eb5ed0bf76c2d588990a5c6b9d9d46d251c5ab9f
Vladyslav Tymofeiev <vladyslavty@interlink-ua.com>
2018-09-12 11:53:20 +0300

    add CustomFieldView test

---
+++ client/src/modules/lead/EditLead/EditLeadSidebar/EditCard/CardFields/EditView/CustomFieldView/CustomFieldView.test.tsx
@@ -0,0 +1,24 @@
+import { shallow } from 'enzyme';
+import 'jsdom-global/register';
+import * as React from 'react';
+import CustomFieldView from './CustomFieldView';
+
+describe('<CustomFieldView/>', () => {
+  const settings = {
+    _id: '5b97a9bb8ef7eb47231396ad',
+    isAlwaysShownInAddDialog: false,
+    isAlwaysVisible:true,
+    isDefault: true,
+    model: 'Contact',
+    name: 'Phone',
+    type: 'string',
+  };
+  
+  let wrapper;
+
+  it('renders without crashing', () => {
+    wrapper = shallow(<CustomFieldView customSettings={settings}/>);
+    expect(wrapper.length).toBe(1);
+  });
+  
+});
