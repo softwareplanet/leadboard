dc866c16e1629b20205e915f71e09530556f368f
Vlad Kozlenko <vladislavko@softwareplanet.uk.com>
2018-09-12 14:03:42 +0300

    reformat domainActions

--- client/src/domain/domainActions.ts
+++ client/src/domain/domainActions.ts
@@ -5,6 +5,9 @@ import { LOAD_DOMAIN } from './types';
 export const loadDomain = (domainId: string) => (dispatch: Dispatch) => {
   axios
     .get(`/api/domain/${domainId}`).then(result => {
-    dispatch({ type: LOAD_DOMAIN, payload: result.data });
+    dispatch({
+      payload: result.data,
+      type: LOAD_DOMAIN,
+    });
   });
 };
