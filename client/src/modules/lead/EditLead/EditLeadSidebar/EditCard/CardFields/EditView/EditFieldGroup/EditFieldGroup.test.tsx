9406f365841a421c1335855f2c2e4b5d78cc4b6d
Valentyn Tulub <valentyntu@softwareplanet.uk.com>
2018-09-12 11:25:54 +0300

    rewrite EditFieldGroup test to typescript

---
+++ client/src/modules/lead/EditLead/EditLeadSidebar/EditCard/CardFields/EditView/EditFieldGroup/EditFieldGroup.test.tsx
@@ -0,0 +1,45 @@
+import { shallow } from 'enzyme';
+import 'jsdom-global/register';
+import * as React from 'react';
+import EditFieldGroup from './EditFieldGroup';
+import * as styles from './EditFieldGroup.css';
+
+const customField = {
+  fieldKey: '5b86a96eed17641891c5011b',
+  name: 'Address',
+  value: 'Grushevskoho st.',
+};
+
+let fieldGroupWrapper: any;
+const handleChange = jest.fn();
+beforeEach(() => {
+  fieldGroupWrapper = shallow(
+    <EditFieldGroup
+      fieldKey={customField.fieldKey}
+      value={customField.value}
+      name={customField.name} onChange={handleChange}
+    />);
+});
+
+describe('EditFieldGroup', () => {
+  it('renders without crashing', () => {
+    expect(fieldGroupWrapper.find('input').hasClass(styles.input)).toBeTruthy();
+  });
+
+  it('renders props', () => {
+    expect(fieldGroupWrapper.find('span').text()).toBe(customField.name);
+    expect(fieldGroupWrapper.find('input').props().defaultValue).toBe(customField.value);
+  });
+
+  it('reacts correctly on change', () => {
+    const input = fieldGroupWrapper.find('input');
+    const newValue = 'Gogolya st.';
+    input.simulate('change', {
+      target: {
+        id: `input${customField.fieldKey}`,
+        value: newValue,
+      },
+    });
+    expect(handleChange).toHaveBeenCalledTimes(1);
+  });
+});
