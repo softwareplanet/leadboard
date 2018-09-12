d88420fa18e4bcceb82b83e76435690967beb0f5
Valentyn Tulub <valentyntu@softwareplanet.uk.com>
2018-09-12 10:38:38 +0300

    rewrite CardField test to typescript

---
+++ client/src/modules/lead/EditLead/EditLeadSidebar/EditCard/CardFields/CardField.test.tsx
@@ -0,0 +1,39 @@
+import { shallow } from 'enzyme';
+import { noop } from 'lodash';
+import * as React from 'react';
+import CustomFieldData from '../../../../../../models/customFields/CustomFieldData';
+import CardField from './CardField';
+
+describe('<CardField/>', () => {
+  let field: CustomFieldData;
+  let wrapper;
+
+  beforeEach(() => {
+    field = {
+      isAlwaysShownInAddDialog: false,
+      isAlwaysVisible: false,
+      isDefault: false,
+      key: '5b97a9aeb35f5710b39e3951',
+      model: 'Contact',
+      name: 'Phone',
+      type: 'string',
+      value: '+380930527927',
+    };
+    wrapper = shallow(<CardField field={field} onUpdate={noop} />);
+  });
+
+  it('renders CardField component', () => {
+    expect(wrapper.exists()).toBeTruthy();
+  });
+
+  it('renders correct props', () => {
+    expect(wrapper.find('span.customFieldLabel').text()).toBe(field.name);
+    expect(wrapper.find('span.customFieldValue').text()).toBe(field.value);
+  });
+
+  it('renders add value link if value is empty', () => {
+    field.value = '';
+    wrapper = shallow(<CardField field={field} onUpdate={noop} />);
+    expect(wrapper.find('span.addValue').length).toBe(1);
+  });
+});
