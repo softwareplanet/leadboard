89743aac7ddbd690168254799f4c7ad26f4c1ce9
SamardakOleh <olehsa@softwareplanet.uk.com>
2018-09-13 16:28:09 +0300

    add heading styles

--- client/src/modules/settings/Funnels/Funnel/Funnel.css.d.ts
+++ client/src/modules/settings/Funnels/Funnel/Funnel.css.d.ts
@@ -1,2 +1,4 @@
 export const funnel: string;
 export const name: string;
+export const heading: string;
+export const edit: string;
--- client/src/modules/settings/Funnels/Funnel/Funnel.tsx
+++ client/src/modules/settings/Funnels/Funnel/Funnel.tsx
@@ -12,7 +12,12 @@ export default class Funnel extends React.Component<Props> {
   public render() {
     return (
       <div className={styles.funnel}>
-        <div className={styles.name}>{this.props.funnel ? this.props.funnel.name : ''}</div>
+        <div>
+          <div className={styles.heading}>
+            <div className={styles.name}>{this.props.funnel ? this.props.funnel.name : ''}</div>
+            <div className={styles.edit}>Edit</div>
+          </div>
+        </div> 
         <Stages />
       </div>
     )
