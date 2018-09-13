9bd346a6a7146165833398e7851834e31cf93135
SamardakOleh <olehsa@softwareplanet.uk.com>
2018-09-13 09:54:57 +0300

    add custom fields editing action. remove redundant files.

--- client/src/modules/settings/customFieldsActions.ts
+++ client/src/modules/settings/customFieldsActions.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { Dispatch } from 'redux';
 import CustomFieldSetting from '../../models/customFields/CustomFieldSetting';
-import { ADD_CUSTOM_FIELD } from './customFieldsActionsTypes';
+import { LOAD_DOMAIN } from './domain/types';
 
 export const addCustomFieldToDomain = 
   (domainId: string, customField: CustomFieldSetting) => 
@@ -11,7 +11,20 @@ export const addCustomFieldToDomain =
       .then(result => {
         dispatch({
           payload: result.data,
-          type: ADD_CUSTOM_FIELD,
+          type: LOAD_DOMAIN,
+        });
+    });
+};
+
+export const editCustomFieldInDomain = 
+  (domainId: string, customField: CustomFieldSetting) => 
+  (dispatch: Dispatch) => {
+    axios
+      .patch(`/api/${domainId}/settings/customFields/${customField._id}`, customField)
+      .then(result => {
+        dispatch({
+          payload: result.data,
+          type: LOAD_DOMAIN,
         });
     });
 };
--- client/src/modules/settings/customFieldsActionsTypes.ts
+++
@@ -1,2 +0,0 @@
-export const ADD_CUSTOM_FIELD: string = 'ADD_CUSTOM_FIELD';
-export const EDIT_CUSTOM_FIELD: string = 'EDIT_CUSTOM_FIELD';
