32833fe1a640aedcceed5af8d223f9adeee7a02c
Vladyslav Tymofeiev <vladyslavty@interlink-ua.com>
2018-09-13 15:23:10 +0300

    add stages and stages view components

---
+++ client/src/modules/settings/Funnels/Funnel/Stages/StageView/StageView.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import * as styles from './StageView.css'
+
+export default class StageView extends React.Component {
+
+  public render() {
+    return (
+      <div className={styles.container}>
+        <div className={styles.content}>
+        </div>
+    </div>
+  )
+  }
+}
--- client/src/modules/settings/Funnels/Funnel/Stages/Stages.tsx
+++ client/src/modules/settings/Funnels/Funnel/Stages/Stages.tsx
@@ -1,10 +1,17 @@
 import * as React from 'react';
+import * as styles from './Stages.css'
+import StageView from './StageView/StageView';
 
 export default class Stages extends React.Component {
 
   public render() {
     return (
-      <div></div>
+      <div className={styles.content}>
+          <div className={styles.stagesList}>
+              <StageView/>
+              <StageView/>
+          </div>
+      </div>
     )
   }
 }
