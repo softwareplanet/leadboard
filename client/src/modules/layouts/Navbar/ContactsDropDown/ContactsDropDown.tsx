91311f766b819bc92aa5b5068c5dfa2bf047cfa7
Vlad Kozlenko <vladislavko@softwareplanet.uk.com>
2018-09-14 10:57:55 +0300

    Use reactstrap for contact drop down remove bootstrap scripts from index.html

--- client/src/modules/layouts/Navbar/ContactsDropDown/ContactsDropDown.tsx
+++ client/src/modules/layouts/Navbar/ContactsDropDown/ContactsDropDown.tsx
@@ -1,7 +1,7 @@
-import * as  classNames from 'classnames';
 import * as React from 'react';
 import { Link, RouteComponentProps, withRouter } from 'react-router-dom';
 import ReactSVG from 'react-svg';
+import { Dropdown, DropdownMenu, DropdownToggle } from 'reactstrap';
 import contactIcon from '../../../../assets/contacts-icon.svg';
 import organizationIcon from '../../../../assets/organization-icon.svg';
 import * as styles from './ContactsDropDown.css';
@@ -12,35 +12,60 @@ const organizationsRoute = '/organizations';
 interface Props extends RouteComponentProps<any> {
 }
 
-class ContactsDropDown extends React.Component<Props, object> {
+interface State {
+  isDropdownOpen: boolean;
+}
+
+class ContactsDropDown extends React.Component<Props, State> {
+  public state: State = {
+    isDropdownOpen: false,
+  };
 
   public isActive = () => {
     const { location } = this.props;
     if (location.pathname === '/people' || location.pathname === '/organizations') {
       return styles.activeContacts;
     } else {
-      return undefined;
+      return styles.notActiveContacts;
     }
+  };
+
+  public toggle = () => {
+    this.setState({ isDropdownOpen: !this.state.isDropdownOpen });
   };
 
   public render() {
     return (
       <div className={this.isActive()}>
-        <div className={styles.dropDown}>
-          <button className="dropdown-toggle" type="button" id="dropdownMenuButton"
-                  data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            <span><ReactSVG className={styles.contactIcon} src={contactIcon} /></span>
-            Contacts
-          </button>
-          <div className={classNames('dropdown-menu', styles.dropDownMenu)} aria-labelledby="dropdownMenuButton">
-            <Link className={styles.dropDownItem} to={peopleRoute}>
-              <ReactSVG className={styles.peopleIcon} src={contactIcon} />People
+        <Dropdown
+          className={this.state.isDropdownOpen ? styles.openedDropDown : styles.closedDropDown}
+          isOpen={this.state.isDropdownOpen}
+          toggle={this.toggle}
+        >
+          <DropdownToggle
+            tag={'div'}
+            onClick={this.toggle}
+            data-toggle="dropdown"
+            aria-expanded={this.state.isDropdownOpen}
+          >
+            <div className={styles.dropDownToggle}>
+              <span><ReactSVG className={styles.contactIcon} src={contactIcon} /></span>
+              Contacts
+            </div>
+          </DropdownToggle>
+          <DropdownMenu className={styles.dropDownMenu}>
+            <Link onClick={this.toggle} to={peopleRoute}>
+              <div className={styles.menuItemDataWrapper}>
+                <ReactSVG className={styles.peopleIcon} src={contactIcon} />People
+              </div>
             </Link>
             <Link className={styles.dropDownItem} to={organizationsRoute}>
-              <ReactSVG className={styles.organizationIcon} src={organizationIcon} />Organizations
+              <div className={styles.menuItemDataWrapper}>
+                <ReactSVG className={styles.organizationIcon} src={organizationIcon} />Organizations
+              </div>
             </Link>
-          </div>
-        </div>
+          </DropdownMenu>
+        </Dropdown>
       </div>
     );
   }
