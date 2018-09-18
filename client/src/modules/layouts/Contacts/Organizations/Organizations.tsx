e2f78c3b72a98b0a86c33cc892870b93d6d7d8ce
Vlad Kozlenko <vladislavko@softwareplanet.uk.com>
2018-09-18 17:14:36 +0300

    use Contacts service for People and Organizations tables

--- client/src/modules/layouts/Contacts/Organizations/Organizations.tsx
+++ client/src/modules/layouts/Contacts/Organizations/Organizations.tsx
@@ -1,50 +1,19 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
-import Organization from '../../../../models/Organization';
+import CustomFieldSetting from '../../../../models/customFields/CustomFieldSetting';
+import OrganizationData from '../../../../models/OrganizationData';
 import Table from '../../../../modules/common/Table/Table';
-import * as columnStyles from '../../../common/Table/ColumnStyles';
 import NavBar from '../../Navbar/Navbar';
+import { getTableData } from '../ContactsService';
 import { loadAggregatedOrganizations } from './organizationActions';
 import * as styles from './Organizations.css';
 
 interface Props {
-  organizations: Organization[];
+  organizations: OrganizationData[];
+  domainCustomFields: CustomFieldSetting[];
 
   loadAggregatedOrganizations(): void;
 }
-
-const columns = [
-  {
-    ...columnStyles.basicColumnStyles,
-    dataField: 'name',
-    text: 'Name',
-  },
-  {
-    dataField: 'custom[0].value',
-    ...columnStyles.basicColumnStyles,
-    text: 'Address',
-  },
-  {
-    dataField: 'contacts',
-    ...columnStyles.rightAlignStyles,
-    text: 'People',
-  },
-  {
-    dataField: 'openedLeads',
-    ...columnStyles.rightAlignStyles,
-    text: 'Open leads',
-  },
-  {
-    dataField: 'closedLeads',
-    ...columnStyles.rightAlignStyles,
-    text: 'Closed leads',
-  },
-  {
-    dataField: 'owner.email',
-    ...columnStyles.basicColumnStyles,
-    text: 'Owner',
-  },
-];
 
 class Organizations extends React.Component<Props, object> {
   public componentWillMount = () => {
@@ -62,14 +31,15 @@ class Organizations extends React.Component<Props, object> {
   public render() {
     const { organizations } = this.props;
     const organizationsCount = organizations.length;
-    if (organizationsCount >= 1) {
+    if (this.isExistsContactData(organizations)) {
+      const tableData = getTableData('Organization', organizations, this.props.domainCustomFields);
       return (
         <div>
           <NavBar />
           <p className={styles.organizationsCounter}>{organizationsCount} {this.getHeader(organizationsCount)} </p>
           <Table
-            data={organizations}
-            columns={columns}
+            data={tableData.contactsData}
+            columns={tableData.contactColumns}
           />
         </div>
       );
@@ -88,9 +58,19 @@ class Organizations extends React.Component<Props, object> {
       );
     }
   }
+
+  private isExistsContactData(contacts: OrganizationData[]) {
+    if (contacts.length > 0) {
+      if (contacts[0].owner) {
+        return true;
+      }
+    }
+    return false;
+  }
 }
 
 const mapStateToProps = (state: any) => ({
+  domainCustomFields: state.domain.settings.customFields,
   organizations: state.organizations,
 });
 
--- client/src/modules/layouts/Contacts/People/People.tsx
+++ client/src/modules/layouts/Contacts/People/People.tsx
@@ -1,79 +1,37 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
-import Contact from '../../../../models/Contact';
-import * as columnStyles from '../../../../modules/common/Table/ColumnStyles';
+import ContactData from '../../../../models/ContactData';
+import CustomFieldSetting from '../../../../models/customFields/CustomFieldSetting';
 import Table from '../../../../modules/common/Table/Table';
 import NavBar from '../../Navbar/Navbar';
 import { loadAggregatedContacts } from './contactActions';
 import * as styles from './People.css';
+import { getTableData } from '../ContactsService';
 
 interface Props {
-  contacts: Contact[];
+  contacts: ContactData[];
+  domainCustomFields: CustomFieldSetting[];
 
   loadAggregatedContacts(): void;
 }
 
-const columns = [
-  {
-    dataField: 'custom[0].value',
-    ...columnStyles.basicColumnStyles,
-    text: 'Email',
-  },
-  {
-    dataField: 'name',
-    ...columnStyles.basicColumnStyles,
-    text: 'Name',
-  },
-  {
-    dataField: 'organization.name',
-    ...columnStyles.basicColumnStyles,
-    text: 'Organization',
-  },
-  {
-    dataField: 'custom[1].value',
-    ...columnStyles.basicColumnStyles,
-    text: 'Phone',
-  },
-  {
-    dataField: 'openedLeads',
-    ...columnStyles.rightAlignStyles,
-    text: 'Open leads',
-  },
-  {
-    dataField: 'closedLeads',
-    ...columnStyles.rightAlignStyles,
-    text: 'Closed leads',
-  },
-  {
-    dataField: 'owner.email',
-    ...columnStyles.basicColumnStyles,
-    text: 'Owner',
-  },
-];
 
 class People extends React.Component<Props, object> {
   public componentWillMount = () => {
     this.props.loadAggregatedContacts();
   };
 
-  public getHeader = (count: number) => {
-    if (count > 1) {
-      return 'people';
-    } else {
-      return 'person';
-    }
-  };
-
   public render() {
     const { contacts } = this.props;
     const contactsCount = contacts.length;
-    if (contactsCount >= 1) {
+    if (this.isExistsContactData(contacts)) {
+      const tableData = getTableData('Contact', contacts, this.props.domainCustomFields);
       return (<div>
         <NavBar />
         <p className={styles.peopleCounter}>{contactsCount} {this.getHeader(contactsCount)}</p>
         <Table
-          data={contacts}
-          columns={columns}
+          data={tableData.contactsData}
+          columns={tableData.contactColumns}
         />
       </div>);
     }
@@ -90,10 +48,28 @@ class People extends React.Component<Props, object> {
       </div>
     );
   }
+
+  private isExistsContactData(contacts: ContactData[]) {
+    if (contacts.length > 0) {
+      if (contacts[0].owner) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  private getHeader = (count: number) => {
+    if (count > 1) {
+      return 'people';
+    } else {
+      return 'person';
+    }
+  };
 }
 
 const mapStateToProps = (state: any) => ({
   contacts: state.contacts,
+  domainCustomFields: state.domain.settings.customFields,
 });
 
 export default connect(mapStateToProps, { loadAggregatedContacts })(People);
