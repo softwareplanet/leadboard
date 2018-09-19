be7ff11b63fab37446f1b212caf0a09c52c123ae
Valentyn Tulub <valentyntu@softwareplanet.uk.com>
2018-09-19 11:57:08 +0300

    create basic AdditionalActionsPopover

---
+++ client/src/modules/lead/EditLead/EditLeadHeader/AdditionalActionsPopover/AdditionalActionsPopover.tsx
@@ -0,0 +1,46 @@
+import * as React from 'react';
+import { Popover } from 'reactstrap';
+import * as styles from './AdditionalActionsPopover.css';
+
+interface Props {
+  target: string;
+  isOpen: boolean;
+
+  toggle(): void;
+}
+
+class AdditionalActionsPopover extends React.Component<Props> {
+
+  public render() {
+    return (
+      <Popover
+        className={styles.popover}
+        placement="bottom-end"
+        target={this.props.target ? this.props.target : ''}
+        isOpen={this.props.isOpen}
+        toggle={this.props.toggle}
+      >
+        <div className={styles.popoverBody}>
+          <div className={styles.actions}>
+            <div
+              className={styles.action}
+              onClick={this.handleDeleteClick}
+            >
+              Delete
+            </div>
+          </div>
+        </div>
+      </Popover>
+    );
+  }
+
+  private handleDeleteClick = () => {
+    if (window.confirm('Are you sure you want to delete this deal?')) {
+      alert('Add delete action!!!!');
+    } else {
+      this.props.toggle();
+    }
+  };
+}
+
+export default AdditionalActionsPopover;
