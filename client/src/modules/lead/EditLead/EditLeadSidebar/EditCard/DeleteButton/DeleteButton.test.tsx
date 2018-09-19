3c9fe86ba4322ed6b48b48d19750bbf91347b7b0
Vlad Kozlenko <vladislavko@softwareplanet.uk.com>
2018-09-19 11:38:30 +0300

    remove redundant test from CustomFieldCard add DeleteButton tests

---
+++ client/src/modules/lead/EditLead/EditLeadSidebar/EditCard/DeleteButton/DeleteButton.test.tsx
@@ -0,0 +1,22 @@
+import { shallow } from 'enzyme';
+import 'jsdom-global/register';
+import * as React from 'react';
+import DeleteButton from './DeleteButton';
+
+describe('<DeleteButton />', () => {
+  let wrapper: any;
+  const onDelete = jest.fn();
+  beforeEach(() => {
+    wrapper = shallow(<DeleteButton onClick={onDelete} className={'deleteButton'} />);
+  });
+
+  it('should render deleteButton component', () => {
+    expect(wrapper.exists()).toBeTruthy();
+  });
+
+  it('should call deleteButton method on click delete button', () => {
+    const button = wrapper.find('.deleteButton');
+    button.simulate('click');
+    expect(onDelete).toHaveBeenCalled();
+  });
+});
