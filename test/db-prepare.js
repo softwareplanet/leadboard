5ad229db85c69e1b5749cc50aae4876691e8d6d0
Vlad Kozlenko <vladislavko@softwareplanet.uk.com>
2018-09-21 12:53:35 +0300

    add note  rest api tests add createNote to db-prepare remove redundant tests from lead rest api tests

--- test/db-prepare.js
+++ test/db-prepare.js
@@ -125,14 +125,19 @@ export async function createContact(app, token, organization, name = "John D") {
   return body;
 }
 
-export async function createNote(app, token, leadId, user, text = "Note 1") {
+export async function createNote(app, token, lead, user, text = "Note 1") {
   const { body } = await request(app())
-    .post(`/api/lead/${leadId}/notes`)
+    .post(`/api/note`)
     .set("Authorization", token)
-    .send({ text, user })
+    .send({
+      text:text,
+      user:user,
+      lead: lead._id,
+      contact: lead.contact._id,
+    })
     .catch(error => {
-      console.log("Cannot create a note" + error);
       throw "Cannot create a note";
     });
+  console.log(JSON.stringify(body));
   return body;
 }
