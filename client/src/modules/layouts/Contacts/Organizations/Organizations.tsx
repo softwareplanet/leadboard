3d600bb6097a991cfd06304a75c4b5a2e0634a93
Vlad Kozlenko <vladislavko@softwareplanet.uk.com>
2018-09-21 09:29:49 +0300

    Extend people and organizations from ContactsPageTemplate rename People.css to ContactsPageTemplate

--- client/src/modules/layouts/Contacts/Organizations/Organizations.tsx
+++ client/src/modules/layouts/Contacts/Organizations/Organizations.tsx
@@ -1,69 +1,27 @@
-import * as React from 'react';
 import { connect } from 'react-redux';
-import CustomFieldSetting from '../../../../models/customFields/CustomFieldSetting';
-import OrganizationData from '../../../../models/OrganizationData';
-import Table from '../../../../modules/common/Table/Table';
-import NavBar from '../../Navbar/Navbar';
-import { getTableData } from '../ContactsService';
-import { isContactsDataValid } from '../ContactsUtils';
+import ContactsPage from '../ContactsPageTemplate';
 import { loadAggregatedOrganizations } from './organizationActions';
-import * as styles from './Organizations.css';
 
-interface Props {
-  organizations: OrganizationData[];
-  domainCustomFields: CustomFieldSetting[];
-
-  loadAggregatedOrganizations(): void;
-}
-
-class Organizations extends React.Component<Props, object> {
-  public componentWillMount = () => {
-    this.props.loadAggregatedOrganizations();
-  };
-
-  public getHeader = (count: number) => {
-    if (count > 1) {
-      return 'organizations';
-    } else {
-      return 'organization';
-    }
-  };
-
-  public render() {
-    const { organizations } = this.props;
-    const organizationsCount = organizations.length;
-    if (isContactsDataValid(organizations)) {
-      const tableData = getTableData('Organization', organizations, this.props.domainCustomFields);
-      return (
-        <div>
-          <NavBar />
-          <p className={styles.organizationsCounter}>{organizationsCount} {this.getHeader(organizationsCount)} </p>
-          <Table
-            data={tableData.contactsData}
-            columns={tableData.contactColumns}
-          />
-        </div>
-      );
-    } else {
-      return (
-        <div>
-          <NavBar />
-          <div>
-            <hr />
-            <div className={styles.warningMessage}>
-              <p>No organizations added yet</p>
-            </div>
-          </div>
-          <hr />
-        </div>
-      );
-    }
-  }
+export class OrganizationPage extends ContactsPage {
+  public modelName = 'Organization';
+  public noContactsMessage = 'No organizations added yet';
+  public oneContactHeader = 'organization';
+  public moreThanOneContactsHeader = 'organizations';
+  public columns = [
+    {
+      dataField: 'name',
+      text: 'Name',
+    },
+    {
+      dataField: 'contacts',
+      text: 'People',
+    },
+  ];
 }
 
 const mapStateToProps = (state: any) => ({
+  contacts: state.organizations,
   domainCustomFields: state.domain.settings.customFields,
-  organizations: state.organizations,
 });
 
-export default connect(mapStateToProps, { loadAggregatedOrganizations })(Organizations);
+export default connect(mapStateToProps, { loadAggregatedContacts: loadAggregatedOrganizations })(OrganizationPage);
--- client/src/modules/layouts/Contacts/People/People.tsx
+++ client/src/modules/layouts/Contacts/People/People.tsx
@@ -1,62 +1,22 @@
-import * as React from 'react';
 import { connect } from 'react-redux';
-import ContactData from '../../../../models/ContactData';
-import CustomFieldSetting from '../../../../models/customFields/CustomFieldSetting';
-import Table from '../../../../modules/common/Table/Table';
-import NavBar from '../../Navbar/Navbar';
-import { getTableData } from '../ContactsService';
-import { isContactsDataValid } from '../ContactsUtils';
+import ContactsPage from '../ContactsPageTemplate';
 import { loadAggregatedContacts } from './contactActions';
-import * as styles from './People.css';
 
-interface Props {
-  contacts: ContactData[];
-  domainCustomFields: CustomFieldSetting[];
-
-  loadAggregatedContacts(): void;
-}
-
-
-class People extends React.Component<Props, object> {
-  public componentWillMount = () => {
-    this.props.loadAggregatedContacts();
-  };
-
-  public render() {
-    const { contacts } = this.props;
-    const contactsCount = contacts.length;
-    if (isContactsDataValid(contacts)) {
-      const tableData = getTableData('Contact', contacts, this.props.domainCustomFields);
-      return (<div>
-        <NavBar />
-        <p className={styles.peopleCounter}>{contactsCount} {this.getHeader(contactsCount)}</p>
-        <Table
-          data={tableData.contactsData}
-          columns={tableData.contactColumns}
-        />
-      </div>);
-    }
-    return (
-      <div>
-        <NavBar />
-        <div>
-          <hr />
-          <div className={styles.warningMessage}>
-            <p>No people added yet</p>
-          </div>
-        </div>
-        <hr />
-      </div>
-    );
-  }
-
-  private getHeader = (count: number) => {
-    if (count > 1) {
-      return 'people';
-    } else {
-      return 'person';
-    }
-  };
+export class People extends ContactsPage {
+  public modelName = 'Contact';
+  public noContactsMessage = 'No people added yet';
+  public oneContactHeader = 'person';
+  public moreThanOneContactsHeader = 'people';
+  public columns = [
+    {
+      dataField: 'name',
+      text: 'Name',
+    },
+    {
+      dataField: 'organization.name',
+      text: 'Organization',
+    },
+  ];
 }
 
 const mapStateToProps = (state: any) => ({
