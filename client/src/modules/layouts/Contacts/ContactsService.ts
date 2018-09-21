42ba62e6cb343beacbfefc087ebc082a5395c14f
Vlad Kozlenko <vladislavko@softwareplanet.uk.com>
2018-09-21 09:26:43 +0300

    rebuild ContactsService

--- client/src/modules/layouts/Contacts/ContactsService.ts
+++ client/src/modules/layouts/Contacts/ContactsService.ts
@@ -1,116 +1,70 @@
+import ContactData from '../../../models/ContactData';
 import CustomField from '../../../models/customFields/CustomField';
 import CustomFieldSetting from '../../../models/customFields/CustomFieldSetting';
 import { basicColumnStyles, rightAlignStyles } from '../../common/Table/ColumnStyles';
 
-interface ContactDataField {
-  name: string;
-  value: string;
-}
-
-interface ContactColumn {
+export interface ContactColumn {
   dataField: string;
+  text: string;
 
-  [name: string]: string;
+  [name: string]: string | object;
 }
 
 interface Styles {
   [name: string]: object;
 }
 
-interface ContactData {
-  [name: string]: string;
-}
-
-const contactModel = 'Contact';
 const columnsWithRightAlignment = ['Closed leads', 'Opened leads', 'People'];
 
-export function getTableData(model: string, contacts: any[], customFieldSetting: CustomFieldSetting[]) {
-  const contactsDataFields = contacts.map((contact: any) => {
-    const customFields = getCustomFieldsByDomainSettings(model, customFieldSetting, contact.custom);
-    return createContactDataFieldsEntity(model, contact, customFields);
+export const injectCustomFields = (contact: ContactData) => {
+  contact.custom.forEach((field: CustomField) => {
+    contact[field.key] = field.value;
   });
-  const contactsData = contactsDataFields.map((dataFields: ContactDataField[]) => createContactData(dataFields));
-  const firstContactFieldsForTemplate = contactsDataFields[0];
-  const contactColumns = firstContactFieldsForTemplate.map((data: ContactDataField) => createColumn(data));
+  return contact;
+};
 
+export function composeTableColumns(
+  modelColumns: ContactColumn[],
+  customFieldSettings: CustomFieldSetting[],
+): ContactColumn[] {
+  return [
+    ...modelColumns,
+    ...customFieldSettings.map(fieldSettingToColumn),
+    {
+      dataField: 'closedLeads',
+      text: 'Closed leads',
+    },
+    {
+      dataField: 'openedLeads',
+      text: 'Opened leads',
+
+    },
+    {
+      dataField: 'owner.email',
+      text: 'Owner',
+    },
+  ].map(injectStyles);
+}
+
+export function getTableData(model: string, contacts: any[], customFieldSettings: CustomFieldSetting[], modelColumns: any[]) {
+  const rows = contacts.map(injectCustomFields);
+  const modelCustomFieldSettings = customFieldSettings.filter(field => field.model === model);
+  const columns = composeTableColumns(modelColumns, modelCustomFieldSettings);
   return {
-    contactColumns,
-    contactsData,
+    columns,
+    rows,
   };
 }
 
-function createContactDataFieldsEntity(model: string, contact: any, customs: ContactDataField[]): ContactDataField[] {
-  if (model === contactModel) {
-    return [
-      {
-        name: 'Name',
-        value: contact.name,
-      },
-      {
-        name: 'Organization',
-        value: contact.organization.name,
-      },
-      ...customs,
-      {
-        name: 'Closed leads',
-        value: contact.closedLeads,
-      },
-      {
-        name: 'Opened leads',
-        value: contact.openedLeads,
-      },
-      {
-        name: 'Owner',
-        value: contact.owner.email,
-      },
-    ];
-  } else {
-    return [
-      {
-        name: 'Name',
-        value: contact.name,
-      },
-      ...customs,
-      {
-        name: 'People',
-        value: contact.contacts,
-      },
-      {
-        name: 'Closed leads',
-        value: contact.closedLeads,
-      },
-      {
-        name: 'Opened leads',
-        value: contact.openedLeads,
-      },
-      {
-        name: 'Owner',
-        value: contact.owner.email,
-      },
-    ];
-  }
-}
+const injectStyles = (column: ContactColumn) => {
+  return Object.assign(column, getStyles(column.text));
+};
 
-function createContactData(dataFields: ContactDataField[]): ContactData {
-  const contactData = {};
-  dataFields.map((dataField: ContactDataField) => {
-    contactData[dataField.name] = dataField.value;
-  });
-  return contactData;
-}
-
-function getCustomFieldsByDomainSettings(
-  model: string,
-  customFieldsSettings: CustomFieldSetting[],
-  modelCustomFields: CustomField[],
-): ContactDataField[] {
-  return customFieldsSettings.filter(field => field.model === model).map(field => {
-    const nextField = modelCustomFields.find(modelField => modelField.key === field._id);
-    return {
-      name: field.name,
-      value: nextField ? nextField.value : '',
-    };
-  });
+function fieldSettingToColumn(field: CustomFieldSetting): ContactColumn {
+  return {
+    dataField: field._id || '',
+    text: field.name,
+  };
 }
 
 function getStyles(name: string): Styles {
@@ -118,12 +72,4 @@ function getStyles(name: string): Styles {
     return { ...rightAlignStyles };
   }
   return { ...basicColumnStyles };
-}
-
-function createColumn(data: ContactDataField): ContactColumn {
-  return {
-    dataField: data.name,
-    ...getStyles(data.name),
-    text: data.name,
-  };
 }
