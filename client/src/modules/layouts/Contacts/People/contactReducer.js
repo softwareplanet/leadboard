cdba5a4353f1c7d20c1ced55049dd44371a22c34
Valentyn Tulub <valentyntu@softwareplanet.uk.com>
2018-09-24 16:37:32 +0300

    add action creator and reducer for adding contact

--- client/src/modules/layouts/Contacts/People/contactReducer.js
+++ client/src/modules/layouts/Contacts/People/contactReducer.js
@@ -1,10 +1,14 @@
-import { LOAD_CONTACTS } from "./types";
+import { ADD_CONTACT, LOAD_CONTACTS } from "./types";
 const initialState = [];
 
 export default function(state = initialState, action) {
   switch (action.type) {
     case LOAD_CONTACTS:
       return action.payload;
+    case ADD_CONTACT:
+      let newContacts = [...state];
+      newContacts.push(action.payload);
+      return newContacts;
     default:
       return state;
   }
