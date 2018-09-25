74d46508c249fe035392106d7e4c7ebe1449ac2e
Valentyn Tulub <valentyntu@softwareplanet.uk.com>
2018-09-25 15:20:57 +0300

    remove redundant reducer part

--- client/src/modules/layouts/Contacts/People/contactReducer.js
+++ client/src/modules/layouts/Contacts/People/contactReducer.js
@@ -1,16 +1,11 @@
-import { ADD_CONTACT, LOAD_CONTACTS } from "./types";
+import { LOAD_CONTACTS } from "./types";
 import { combineReducers } from "redux";
-import activityReducer from "../../../lead/EditLead/Activities/activityReducer";
 import detailedContactReducer from "../../../detailedView/Contact/detailedContactReducer";
 
 const contactsReducer = (state = [], action) => {
   switch (action.type) {
     case LOAD_CONTACTS:
       return action.payload;
-    case ADD_CONTACT:
-      let newContacts = [...state];
-      newContacts.push(action.payload);
-      return newContacts;
     default:
       return state;
   }
