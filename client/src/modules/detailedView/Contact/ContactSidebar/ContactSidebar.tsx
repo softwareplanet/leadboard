e9a802f7ed869b3b8f49d8ad4174947049b65226
Vladyslav Tymofeiev <vladyslavty@interlink-ua.com>
2018-09-25 10:08:48 +0300

    add empty organization card with autocomplete to ContactSidebar

--- client/src/modules/detailedView/Contact/ContactSidebar/ContactSidebar.tsx
+++ client/src/modules/detailedView/Contact/ContactSidebar/ContactSidebar.tsx
@@ -1,14 +1,19 @@
+import classNames from 'classnames';
 import * as React from 'react';
 import { connect } from 'react-redux';
 import Contact from '../../../../models/Contact';
 import CustomFieldSetting from '../../../../models/customFields/CustomFieldSetting';
 import DomainSettings from '../../../../models/DomainSettings';
 import Organization from '../../../../models/Organization';
+import OrganizationAutocomplete from '../../../common/autocomplete/organization/OrganizationAutocomplete';
+import { autocompleteStyles } from '../../../common/autocomplete/styles/autocomplete-styles';
 import {
   getCustomFieldSettingsByModel,
   makeCustomFieldData,
 } from '../../../lead/EditLead/EditLeadSidebar/CustomFieldsService';
 import EditCard from '../../../lead/EditLead/EditLeadSidebar/EditCard/EditCard';
+import * as editCardStyles from '../../../lead/EditLead/EditLeadSidebar/EditCard/EditCard.css';
+import EmptyCard from '../../../lead/EditLead/EditLeadSidebar/EmptyCard/EmptyCard';
 import { updateContact } from '../../../lead/leadActions';
 import { addCustomFieldToDomain, editCustomFieldInDomain } from '../../../settings/customFieldsActions';
 import { deleteCustomField } from '../../../settings/domain/domainActions';
@@ -47,7 +52,7 @@ class ContactSidebar extends React.Component<Props> {
       <EditCard
         deleteCustomField={this.deleteCustomField}
         model={contact}
-        title="Person"
+        title="Details"
         modelType="Contact"
         addCustomFieldToDomain={this.props.addCustomFieldToDomain}
         editCustomFieldInDomain={this.props.editCustomFieldInDomain}
@@ -56,9 +61,22 @@ class ContactSidebar extends React.Component<Props> {
         customFields={makeCustomFieldData('Contact', contact, settings)}
       />;
 
+    const emptyOrganizationCard = (
+      <EmptyCard
+        id="organization-card"
+        title="Organization"
+        styles={autocompleteStyles.linkOrganization}
+        iTagClass={classNames('fas fa-building', editCardStyles.inputIcon)}
+        items={this.props.organizations}
+      >
+        <OrganizationAutocomplete />
+      </EmptyCard>
+    );
+
     return (
       <div className={detailedViewStyles.sidebar}>
-        {contactCard}
+        { contactCard }
+        { emptyOrganizationCard }
       </div>
     );
   }
