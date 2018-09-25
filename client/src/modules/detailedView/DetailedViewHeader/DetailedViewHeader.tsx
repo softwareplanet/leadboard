690ae1506f3d0625a504194550049091eda4a084
SamardakOleh <olehsa@softwareplanet.uk.com>
2018-09-25 13:11:43 +0300

    add owner icon to header with styles

--- client/src/modules/detailedView/DetailedViewHeader/DetailedViewHeader.tsx
+++ client/src/modules/detailedView/DetailedViewHeader/DetailedViewHeader.tsx
@@ -1,14 +1,20 @@
 import * as React from 'react';
+import ownerIcon from '../../../assets/img/user-icon.svg';
 import organizationIcon from '../../../assets/img/organizationIcon.svg';
 import personIcon from '../../../assets/img/personIcon.svg';
 import Contact from '../../../models/Contact';
 import Organization from '../../../models/Organization';
 import * as styles from './DetailedViewHeader.css';
+import * as leadHeaderStyles from '../../lead/EditLead/EditLeadHeader/EditLeadHeader.css';
 
 interface Props {
   model: Organization | Contact;
   modelType: string;
 }
+
+const displayFlex = {
+  display: 'flex',
+};
 
 export default class DetailedViewHeader extends React.Component<Props> {
 
@@ -16,10 +22,21 @@ export default class DetailedViewHeader extends React.Component<Props> {
     const iconSrc = this.props.modelType === 'Organization' ? organizationIcon : personIcon;
     return (
       <div className={styles.header}>
-        <span className={styles.badge}>
-          <img className={styles.icon} src={iconSrc} alt="Icon" />
-        </span>
-        <h1 className={styles.name}>{this.props.model.name}</h1>
+        <div style={displayFlex}>
+          <span className={styles.badge}>
+            <img className={styles.icon} src={iconSrc} alt="Icon" />
+          </span>
+          <h1 className={styles.name}>{this.props.model.name}</h1>
+        </div>
+        <div className={leadHeaderStyles.leadOptions}>
+          <div className={leadHeaderStyles.owner}>
+            <img src={ownerIcon} className={leadHeaderStyles.ownerPicture} />
+            <div className={leadHeaderStyles.ownerBody}>
+              {/* <span>{editLead ? editLead.owner.firstname + '' + editLead.owner.lastname : null}</span> */}
+              <small className={leadHeaderStyles.ownerRole}>Owner</small>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
