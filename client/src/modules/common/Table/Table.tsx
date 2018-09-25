822262f3add7ce8c4f9ae7ae331549e826afef0d
Vlad Kozlenko <vladislavko@softwareplanet.uk.com>
2018-09-25 10:13:45 +0300

    add history push to entityPage on row select add modelName in Table's props

--- client/src/modules/common/Table/Table.tsx
+++ client/src/modules/common/Table/Table.tsx
@@ -1,12 +1,23 @@
 import * as React from 'react';
 import BootstrapTable from 'react-bootstrap-table-next';
+import history from '../../../history';
 
 interface Props {
+  modelName: string;
   data: any[];
   columns: any[];
 }
 
 class Table extends React.Component<Props, object> {
+
+  public selectRow = {
+    clickToSelect: true,
+    hideSelectColumn: true,
+    onSelect: (row: any) => {
+      history.push(`/${this.props.modelName.toLocaleLowerCase()}/${row._id}`);
+    },
+  };
+
   public render() {
     return (
       <div>
@@ -15,6 +26,7 @@ class Table extends React.Component<Props, object> {
           data={this.props.data}
           columns={this.props.columns}
           hover={true}
+          selectRow={this.selectRow}
         />
       </div>
     );
