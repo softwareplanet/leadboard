2da962ba11c378062efdb746c521092504564773
Vladyslav Tymofeiev <vladyslavty@interlink-ua.com>
2018-09-26 11:38:06 +0300

    add organization card to contact's sidebar

--- client/src/modules/detailedView/Contact/ContactSidebar/ContactSidebar.tsx
+++ client/src/modules/detailedView/Contact/ContactSidebar/ContactSidebar.tsx
@@ -1,28 +1,34 @@
 import classNames from 'classnames';
+import { isEmpty } from 'lodash';
 import * as React from 'react';
 import { connect } from 'react-redux';
+import organizationIcon from '../../../../assets/img/organizationIcon.svg';
 import Contact from '../../../../models/Contact';
 import CustomFieldSetting from '../../../../models/customFields/CustomFieldSetting';
 import DomainSettings from '../../../../models/DomainSettings';
 import Organization from '../../../../models/Organization';
 import OrganizationAutocomplete from '../../../common/autocomplete/organization/OrganizationAutocomplete';
 import { autocompleteStyles } from '../../../common/autocomplete/styles/autocomplete-styles';
-import {
-  getCustomFieldSettingsByModel,
-  makeCustomFieldData,
-} from '../../../lead/EditLead/EditLeadSidebar/CustomFieldsService';
+import { loadOrganizations } from '../../../layouts/Contacts/Organizations/organizationActions';
+import { getCustomFieldSettingsByModel,makeCustomFieldData } from '../../../lead/EditLead/EditLeadSidebar/CustomFieldsService';
 import EditCard from '../../../lead/EditLead/EditLeadSidebar/EditCard/EditCard';
 import * as editCardStyles from '../../../lead/EditLead/EditLeadSidebar/EditCard/EditCard.css';
 import EmptyCard from '../../../lead/EditLead/EditLeadSidebar/EmptyCard/EmptyCard';
-import { updateContact } from '../../../lead/leadActions';
+import { updateOrganization } from '../../../lead/leadActions';
 import { addCustomFieldToDomain, editCustomFieldInDomain } from '../../../settings/customFieldsActions';
 import { deleteCustomField } from '../../../settings/domain/domainActions';
 import * as detailedViewStyles from '../../detailedView.css';
+import { updateContact } from '../detailedContactActions';
 
 
 interface Props {
   settings: DomainSettings;
   organizations?: Organization[];
+  contact: Contact;
+
+  loadOrganizations(): void;
+
+  updateOrganization(organization: Organization): void;
 
   updateContact(contact: Contact): void;
 
@@ -34,33 +40,49 @@ interface Props {
 }
 
 class ContactSidebar extends React.Component<Props> {
+  public componentDidMount() {
+    this.props.loadOrganizations();
+  }
 
   public render() {
-    const { settings } = this.props;
+    const { settings, contact, organizations } = this.props;
+    const { organization } = this.props.contact;
+    let organizationCard;
+    let contactCard;
+    if(contact){
+      contactCard = (
+        <EditCard
+          advancedMode={false}
+          deleteCustomField={this.deleteCustomField}
+          model={contact}
+          title="Details"
+          modelType="Contact"
+          addCustomFieldToDomain={this.props.addCustomFieldToDomain}
+          editCustomFieldInDomain={this.props.editCustomFieldInDomain}
+          onUpdate={this.props.updateContact}
+          customFieldsSettings={getCustomFieldSettingsByModel('Contact', settings)}
+          customFields={makeCustomFieldData('Contact', contact, settings)}
+        />
+      );
+    }
 
-    const contact = {
-      __v: 0,
-      _id: '5ba893074d1ee31a7e7c2b7e',
-      custom: [],
-      domain: '5ba892ff4d1ee31a7e7c2b73',
-      name: 'Bob',
-      owner: '5ba892ff4d1ee31a7e7c2b74',
-      timestamp: new Date(),
-    };
-
-    const contactCard =
-      <EditCard
-        advancedMode={false}
-        deleteCustomField={this.deleteCustomField}
-        model={contact}
-        title="Details"
-        modelType="Contact"
-        addCustomFieldToDomain={this.props.addCustomFieldToDomain}
-        editCustomFieldInDomain={this.props.editCustomFieldInDomain}
-        onUpdate={this.props.updateContact}
-        customFieldsSettings={getCustomFieldSettingsByModel('Contact', settings)}
-        customFields={makeCustomFieldData('Contact', contact, settings)}
-      />;
+    if(organization) {
+      organizationCard = (
+        <EditCard
+          advancedMode={true}
+          deleteCustomField={this.deleteCustomField}
+          model={organization}
+          modelType="Organization"
+          title="Organization"
+          icon={organizationIcon}
+          onUpdate={this.props.updateOrganization}
+          addCustomFieldToDomain={this.props.addCustomFieldToDomain}
+          editCustomFieldInDomain={this.props.editCustomFieldInDomain}
+          customFieldsSettings={getCustomFieldSettingsByModel('Organization', settings)}
+          customFields={makeCustomFieldData('Organization', organization, settings)}
+        />
+      );
+    }
 
     const emptyOrganizationCard = (
       <EmptyCard
@@ -68,7 +90,9 @@ class ContactSidebar extends React.Component<Props> {
         title="Organization"
         styles={autocompleteStyles.linkOrganization}
         iTagClass={classNames('fas fa-building', editCardStyles.inputIcon)}
-        items={this.props.organizations}
+        items={organizations}
+        lead={contact}
+        onUpdate={this.props.updateContact}
       >
         <OrganizationAutocomplete />
       </EmptyCard>
@@ -77,7 +101,7 @@ class ContactSidebar extends React.Component<Props> {
     return (
       <div className={detailedViewStyles.sidebar}>
         { contactCard }
-        { emptyOrganizationCard }
+        { isEmpty(contact.organization) ? emptyOrganizationCard : organizationCard }
       </div>
     );
   }
@@ -93,6 +117,8 @@ class ContactSidebar extends React.Component<Props> {
 }
 
 const mapStateToProps = (state: any) => ({
+  contact: state.contact.detailedContact.contact,
+  organizations: state.organization.organizations,
   settings: state.domain.settings,
 });
 
@@ -104,6 +130,8 @@ export default connect(
     addCustomFieldToDomain,
     deleteCustomField,
     editCustomFieldInDomain,
+    loadOrganizations,
     updateContact,
+    updateOrganization,
   },
 )(ContactSidebar);
