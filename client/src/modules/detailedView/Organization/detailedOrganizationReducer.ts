9f47c73cfdf872db294d9a3a1ec874b621d11ded
Valentyn Tulub <valentyntu@softwareplanet.uk.com>
2018-09-27 12:23:35 +0300

    simplify detailedOrganizationReducer

--- client/src/modules/detailedView/Organization/detailedOrganizationReducer.ts
+++ client/src/modules/detailedView/Organization/detailedOrganizationReducer.ts
@@ -1,8 +1,9 @@
 import { combineReducers } from 'redux';
 import Action from '../../../models/Action';
+import replacementReducer from '../../../replacementReducer';
 import activityReducer from '../../lead/EditLead/Activities/activityReducer';
-import { LOAD_CONTACTS_FOR_ORGANIZATION, LOAD_ORGANIZATION } from './types';
 import noteReducer from '../../lead/EditLead/EditLeadContent/EditLeadHistory/Notes/noteReducer';
+import { LOAD_CONTACTS_FOR_ORGANIZATION, LOAD_ORGANIZATION } from './types';
 
 const detailedOrganizationReducer = (state = {}, action: Action) => {
   switch (action.type) {
@@ -13,18 +14,9 @@ const detailedOrganizationReducer = (state = {}, action: Action) => {
   }
 };
 
-const organizationsContactsReducer = (state = [], action: Action) => {
-  switch (action.type) {
-    case LOAD_CONTACTS_FOR_ORGANIZATION:
-      return action.payload;
-    default:
-      return state;
-  }
-};
-
 export default combineReducers({
   activities: activityReducer,
+  contacts: replacementReducer(LOAD_CONTACTS_FOR_ORGANIZATION, []),
   notes: noteReducer,
-  contacts: organizationsContactsReducer,
   organization: detailedOrganizationReducer,
 });
