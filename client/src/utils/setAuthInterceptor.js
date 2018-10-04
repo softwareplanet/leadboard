838383ad6b97d2c8410065a2976c67c6063de8e5
vladislavkoz <vladislavko@softwareplanet.uk.com>
2018-10-04 17:26:30 +0300

    add check on pipedrive response to AuthInterceptor

--- client/src/utils/setAuthInterceptor.js
+++ client/src/utils/setAuthInterceptor.js
@@ -3,14 +3,14 @@ import { logoutUser } from "../modules/auth/authActions";
 import store from "../store";
 import createHistory from 'history/createBrowserHistory'
 
-const history = createHistory()
+const history = createHistory();
 
 
 const setAuthInterceptor = () => {
   axios.interceptors.response.use(response => {
     return response;
   }, err => {
-    if (err.response.status === 401) {
+    if (err.response.status === 401 && !err.response.config.url.includes('api.pipedrive.com')) {
       store.dispatch(logoutUser());
       history.push("/");
     }
