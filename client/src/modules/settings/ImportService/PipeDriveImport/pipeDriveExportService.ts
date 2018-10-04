55c3c2da304e1c50d6034648f1990adc39c15de7
vladislavkoz <vladislavko@softwareplanet.uk.com>
2018-10-04 19:05:31 +0300

    reformat pipeDriveExportService

--- client/src/modules/settings/ImportService/PipeDriveImport/pipeDriveExportService.ts
+++ client/src/modules/settings/ImportService/PipeDriveImport/pipeDriveExportService.ts
@@ -3,35 +3,24 @@ import axios from 'axios';
 const pipedriveConnectionProblem = 'Pipedrive refused with: ';
 
 export const exportOrganizations = (token: string) => {
-  return axios
-    .get(`https://api.pipedrive.com/v1/organizations?start=0&api_token=${token}`)
-    .then(result => result.data.data)
-    .catch(error => {
-      throw new Error(createErrorMessage(error));
-    });
+  return getDataFromPipedrive(`https://api.pipedrive.com/v1/organizations?start=0&api_token=${token}`);
 };
 
 export const exportContacts = (token: string) => {
-  return axios
-    .get(`https://api.pipedrive.com/v1/persons?start=0&api_token=${token}`)
-    .then(result => result.data.data)
-    .catch(error => {
-      throw new Error(createErrorMessage(error));
-    });
+  return getDataFromPipedrive(`https://api.pipedrive.com/v1/persons?start=0&api_token=${token}`);
 };
 
 export const exportOrganizationFields = (token: string) => {
-  return axios
-    .get(`https://api.pipedrive.com/v1/organizationFields?start=0&api_token=${token}`)
-    .then(result => result.data.data)
-    .catch(error => {
-      throw new Error(createErrorMessage(error));
-    });
+  return getDataFromPipedrive(`https://api.pipedrive.com/v1/organizationFields?start=0&api_token=${token}`);
 };
 
 export const exportContactFields = (token: string) => {
+  return getDataFromPipedrive(`https://api.pipedrive.com/v1/personFields?start=0&api_token=${token}`);
+};
+
+const getDataFromPipedrive = (url: string) => {
   return axios
-    .get(`https://api.pipedrive.com/v1/personFields?start=0&api_token=${token}`)
+    .get(url)
     .then(result => result.data.data)
     .catch(error => {
       throw new Error(createErrorMessage(error));
