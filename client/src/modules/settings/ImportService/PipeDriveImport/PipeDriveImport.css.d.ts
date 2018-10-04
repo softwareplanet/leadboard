ad3f535432f2fb52574e60387ec02928eb4444db
vladislavkoz <vladislavko@softwareplanet.uk.com>
2018-10-04 10:50:42 +0300

    Add styles to PipeDriveImport page

--- client/src/modules/settings/ImportService/PipeDriveImport/PipeDriveImport.css.d.ts
+++ client/src/modules/settings/ImportService/PipeDriveImport/PipeDriveImport.css.d.ts
@@ -2,3 +2,6 @@ export const container: string;
 export const button: string;
 export const title: string;
 export const dataModule: string;
+export const tokenHint: string;
+export const successStyles: string;
+export const rejectStyles: string;
